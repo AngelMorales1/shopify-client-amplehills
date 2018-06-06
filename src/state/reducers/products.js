77711fefec3ac9ad3f707889bbef765185c6661f
Joshie Fishbein <joshie@sanctuary.computer>
2018-06-06 18:33:11 -0400

    cleanup

--- src/state/reducers/products.js
+++ src/state/reducers/products.js
@@ -4,7 +4,6 @@ const initialState = [];
 
 export default (state = initialState, action) => {
   const { type } = action;
-  console.log(action);
   switch (type) {
     case `${FETCH_PRODUCTS}_FULFILLED`:
       return action.payload;
