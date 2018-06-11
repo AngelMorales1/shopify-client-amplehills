8bec841760e86c5f22c9f148f1726faf099a958d
sohee Lim <limsohee1002@gmail.com>
2018-06-11 18:45:37 -0400

    add navigation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,56 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "../../styles/nav.scss";
 
 class Nav extends Component {
   render() {
     return (
-      <div className="p1 mb3">
-        <span>Ample Hills</span>
-        <ul>
-          <li className="inline mr1">
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-          <li className="inline mx1">
-            <NavLink exact to="/collections">
-              Collections
-            </NavLink>
-          </li>
-          <li className="inline mx1">
-            <NavLink exact to="/products">
-              Products
-            </NavLink>
-          </li>
-        </ul>
+      <div>
+        <div className="align-center">
+          <ul>
+            <li className="mr1 text">
+              <NavLink exact to="/">
+                Ample Hills
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="p1 mb3 order-box">
+          <div className="inline align-left">
+            <ul>
+              <li className="inline text margin-left">
+                <NavLink exact to="/Location">
+                  Location
+                </NavLink>
+              </li>
+              <li className="inline text margin-left">
+                <NavLink exact to="/Contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className="inline align-right">
+            <ul>
+              <li className="inline margin-right text">
+                <NavLink exact to="/profile">
+                  Profile
+                </NavLink>
+              </li>
+              <li className="inline margin-right text">
+                <NavLink exact to="/collections">
+                  Collections
+                </NavLink>
+              </li>
+              <li className="inline margin-right text text--box hover-reset">
+                <NavLink exact to="/products">
+                  Shop Online
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
