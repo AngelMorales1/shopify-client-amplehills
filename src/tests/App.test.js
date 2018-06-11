f7b64f9717156e1ae8ec1614c16f84b19cf50c27
Joshie Fishbein <joshie@sanctuary.computer>
2018-06-11 18:03:41 -0400

    add cross-env, add tests to pre-commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { render } from 'enzyme';
+
+import { store } from 'store';
 import App from 'App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
