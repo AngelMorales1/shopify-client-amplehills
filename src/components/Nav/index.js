403d4bcf68ffc96677e3d8d810d9d3e1de40f7a9
sohee Lim <limsohee1002@gmail.com>
2018-06-12 16:56:20 -0400

    combine two classes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,29 +5,17 @@ import styles from './Nav.scss';
 class Nav extends Component {
   render() {
     return (
-      <div className="mx0 my3 px4 nav flex clearfix">
+      <div className="mx0 my3 px4 flex clearfix">
         <div className={`col col-4 flex justify-start ${styles['left-side']}`}>
-          <NavLink
-            exact
-            to="/Location"
-            className="flex ml4 link-text link-text__align"
-          >
+          <NavLink exact to="/Location" className="flex ml4 link-text">
             Location
           </NavLink>
-          <NavLink
-            exact
-            to="/Contact"
-            className="flex ml4 link-text link-text__align"
-          >
+          <NavLink exact to="/Contact" className="flex ml4 link-text">
             Contact
           </NavLink>
         </div>
         <div className="col col-4">
-          <NavLink
-            exact
-            to="/"
-            className="flex justify-center link-text__align"
-          >
+          <NavLink exact to="/" className="flex justify-center">
             <img src="/assets/images/ample-hills-logo.svg" />
           </NavLink>
         </div>
@@ -36,25 +24,13 @@ class Nav extends Component {
             styles['right-side']
           }`}
         >
-          <NavLink
-            exact
-            to="/profile"
-            className="flex mr4 link-text link-text__align"
-          >
+          <NavLink exact to="/profile" className="flex mr4 link-text">
             Profile
           </NavLink>
-          <NavLink
-            exact
-            to="/collections"
-            className="flex mr4 link-text link-text__align"
-          >
+          <NavLink exact to="/collections" className="flex mr4 link-text">
             Collections
           </NavLink>
-          <NavLink
-            exact
-            to="/products"
-            className="flex mr4 link-text link-text__align"
-          >
+          <NavLink exact to="/products" className="flex mr4 link-text">
             <button className="button">Shop Online</button>
           </NavLink>
         </div>
