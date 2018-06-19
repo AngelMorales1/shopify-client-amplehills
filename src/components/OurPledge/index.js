e8e1aa4c1f73a4f640b023e589dbdd3718f0b780
sohee Lim <limsohee1002@gmail.com>
2018-06-19 14:14:54 -0400

    our pledge styling

--- src/components/OurPledge/index.js
+++ src/components/OurPledge/index.js
@@ -2,23 +2,36 @@ import React, { Component } from 'react';
 import cx from 'classnames';
 
 import styles from './OurPledge.scss';
+import { Image } from 'components/base';
 
 class OurPledge extends Component {
   render() {
     return (
-      <div
-        className={cx(
-          'm4 flex clearfix items-center bg-varden',
-          styles['OurPledge-container']
-        )}
-      >
-        <p className="mx-auto col4 px1 text-madison-blue bold">Our Pledge</p>
-        <p className="uppercase mx-auto col4 px1 flex text-madison-blue info-text-small semi-bold">
-          Ice cream arrives fresh delicious, and frozen
-        </p>
-        <p className="uppercase mx-auto col4 px1 text-madison-blue info-text-big bold info-text-focus">
-          More Info
-        </p>
+      <div className="flex flex-column items-center">
+        <Image
+          alt="bubble icon"
+          src="/assets/images/bubble-icon.svg"
+          className={cx('icon', styles['OurPledge-icon'])}
+        />
+        <div
+          className={cx(
+            'bg-varden p2 flex items-center',
+            styles['OurPledge-container']
+          )}
+        >
+          <p className="col4 mx2 text-madison-blue bold nowrap">Our Pledge</p>
+          <p className="col4 mx2 flex uppercase text-madison-blue info-text-small semi-bold">
+            Ice cream arrives fresh delicious, and frozen
+          </p>
+          <p
+            className={cx(
+              'col4 mx2 uppercase text-madison-blue info-text-big bold nowrap',
+              styles['OurPledge__moreinfo']
+            )}
+          >
+            More Info
+          </p>
+        </div>
       </div>
     );
   }
