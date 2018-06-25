075951e91f23536d66bae91eaa331a16ad63ac6a
sohee Lim <limsohee1002@gmail.com>
2018-06-25 15:30:55 -0400

    clean up

--- src/components/base/Radio/index.js
+++ src/components/base/Radio/index.js
@@ -6,24 +6,35 @@ import styles from './Radio.scss';
 
 import { Image, Button } from 'components/base';
 
-const Radio = ({ className, label }) => {
+const Radio = ({ className, label, check, onClick }) => {
   return (
     <Button
       variant="style-none"
       onClick={onClick}
       className={cx(styles['Radio'], 'text-peach bold', className)}
-    />
+    >
+      {check ? (
+        <Image
+          className={cx('fixed', styles['Radio__check'])}
+          alt="Check icon"
+          src="/assets/images/icon-check.svg"
+        />
+      ) : null}
+      {label}
+    </Button>
   );
 };
 
 Radio.propTypes = {
   className: PropTypes.string,
-  label: PropTypes.string
+  label: PropTypes.string,
+  check: PropTypes.bool
 };
 
 Radio.defaultProps = {
   className: '',
-  label: ''
+  label: '',
+  check: false
 };
 
 export default Radio;
