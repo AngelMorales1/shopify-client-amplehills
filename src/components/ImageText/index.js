4a00d2f898b6af864225b84dc048885ccf7b3355
sohee Lim <limsohee1002@gmail.com>
2018-06-28 18:44:42 -0400

    mobile style

--- src/components/ImageText/index.js
+++ src/components/ImageText/index.js
@@ -7,6 +7,7 @@ import imageModel from 'models/imageModel';
 import styles from './ImageText.scss';
 import { Image } from 'components/base';
 
+<<<<<<< HEAD
 const ImageText = ({ block, z }) => {
   const fields = get(block, 'fields', {});
   const colorClass = `ImageText--${get(fields, 'backgroundColor', 'Pink')}`;
@@ -27,6 +28,32 @@ const ImageText = ({ block, z }) => {
         <div className="flex flex-column justify-center my4 col-4">
           <h2 className="block-headline mb3">{get(fields, 'title', '')}</h2>
           <p className="description">{get(fields, 'text', '')}</p>
+=======
+class ImageText extends Component {
+  render() {
+    const { block } = this.props;
+    const fields = get(block, 'fields', {});
+    const colorClass = `ImageText--${get(fields, 'backgroundColor', 'Pink')}`;
+    const position = get(fields, 'imagePosition', 0);
+    return (
+      <div className={cx('flex drip', styles['ImageText'], styles[colorClass])}>
+        <div className={cx(styles['ImageText__container'])}>
+          <Image
+            className={cx(styles['ImageText__image'], 'z-overlay col-4')}
+            style={{ transform: `translateY(${position}%)` }}
+            alt={`${get(fields, 'title', '')} illustration`}
+            src={get(fields, 'image.fields.file.url', '')}
+          />
+          <div
+            className={cx(
+              styles['ImageText__text-content'],
+              'flex flex-column justify-center'
+            )}
+          >
+            <h2 className="block-headline mb3">{get(fields, 'title', '')}</h2>
+            <p className="description">{get(fields, 'text', '')}</p>
+          </div>
+>>>>>>> mobile style
         </div>
       </div>
     </div>
