4e194f62b9b9871e8292cd0ea23a302e4fb15137
sohee Lim <limsohee1002@gmail.com>
2018-07-02 18:58:58 -0400

    render data

--- src/components/ComicStrip/index.js
+++ src/components/ComicStrip/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
 import get from 'utils/get';
 
@@ -22,12 +21,65 @@ class ComicStrip extends Component {
   }
 
   render() {
-    console.log(this.props);
     const { block, z } = this.props;
     const fields = get(block, 'fields', {});
-
+    const products = get(fields, 'products', []);
     const colorClass = `ComicStrip--${get(fields, 'color', 'Yellow')}`;
-    return <div />;
+    return (
+      <div style={{ zIndex: z }} className={cx(styles[colorClass])}>
+        <h2 className="block-headline">The Artwork</h2>
+        {products.map((product, i) => {
+          const color = this.isActiveFlavor(get(product, 'sys.id', ''), i)
+            ? 'denim'
+            : 'white-denim';
+
+          return (
+            <Button
+              className="m1"
+              color={color}
+              key={get(product, 'sys.id', '')}
+              label={get(product, 'fields.title', '')}
+              onClick={() =>
+                this.setState({
+                  activeFlavor: get(product, 'sys.id', '')
+                })
+              }
+            />
+          );
+        })}
+        <div className={`${styles['ComicStrips--container']}`}>
+          {products.map((product, i) => {
+            const comics = get(product, 'fields.comicStrip', []);
+            const classes = cx(
+              styles['ComicStrip'],
+              'container-width mx-auto flex flex-wrap py3',
+              {
+                [styles['ComicStrip--active']]: this.isActiveFlavor(
+                  get(product, 'sys.id', ''),
+                  i
+                )
+              }
+            );
+
+            return (
+              <div className={classes} key={get(product, 'sys.id', '')}>
+                {comics.map((comic, i) => {
+                  console.log(comic);
+                  const comicUrl = get(comic, 'fields.file.url', '');
+                  return (
+                    <Image
+                      key={`${i}-${get(comic, 'sys.id', '')}`}
+                      alt={`${get(comic, 'fields.title', '')}`}
+                      src={comicUrl}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
