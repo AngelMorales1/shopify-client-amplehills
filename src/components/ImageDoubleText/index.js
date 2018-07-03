a8a90de4233544aae256f0066151fed104b5fa2b
sohee Lim <limsohee1002@gmail.com>
2018-07-03 18:31:32 -0400

    style fix

--- src/components/ImageDoubleText/index.js
+++ src/components/ImageDoubleText/index.js
@@ -5,54 +5,50 @@ import get from 'utils/get';
 import styles from './ImageDoubleText.scss';
 import { Image } from 'components/base';
 
-class ImageDoubleText extends Component {
-  render() {
-    const { block, z } = this.props;
-    const fields = get(block, 'fields', {});
-    const colorClass = `ImageDoubleText--${get(
-      fields,
-      'backgroundColor',
-      'Light-Yellow'
-    )}`;
-    return (
+const ImageDoubleText = ({ block, z }) => {
+  const fields = get(block, 'fields', {});
+  const colorClass = `ImageDoubleText--${get(
+    fields,
+    'backgroundColor',
+    'light-yellow'
+  )}`;
+
+  return (
+    <div
+      style={{ zIndex: z }}
+      className={cx(
+        styles['ImageDoubleText'],
+        styles[colorClass],
+        'flex justify-end relative drip'
+      )}
+    >
+      <div className={cx(styles['ImageDoubleText--image'], 'circle absolute')}>
+        <Image
+          alt={`${get(fields, 'image.fields.title', '')} illustration`}
+          src={get(fields, 'image.fields.file.url', '')}
+        />
+      </div>
       <div
-        style={{ zIndex: z }}
         className={cx(
-          styles['ImageDoubleText'],
-          styles[colorClass],
-          'flex justify-end relative drip'
+          styles['ImageDoubleText--text-container'],
+          'flex flex-column justify-center items-center col-12 md-col-5'
         )}
       >
-        <div
-          className={cx(styles['ImageDoubleText--image'], 'circle absolute')}
-        >
-          <Image
-            alt={`${get(fields, 'image.fields.title', '')} illustration`}
-            src={get(fields, 'image.fields.file.url', '')}
-          />
+        <div className="mb3">
+          <h2 className="block-headline mb2">
+            {get(fields, 'firstTitle', '')}
+          </h2>
+          <p className="copy">{get(fields, 'firstText', '')}</p>
         </div>
-        <div
-          className={cx(
-            styles['ImageDoubleText--text-container'],
-            'flex flex-column justify-center items-center col-12 md-col-5'
-          )}
-        >
-          <div className="mb3">
-            <h2 className="block-headline mb2">
-              {get(fields, 'firstTitle', '')}
-            </h2>
-            <p className="copy">{get(fields, 'firstText', '')}</p>
-          </div>
-          <div>
-            <h2 className="block-headline mb2">
-              {get(fields, 'secondTitle', '')}
-            </h2>
-            <p className="copy">{get(fields, 'secondText', '')}</p>
-          </div>
+        <div>
+          <h2 className="block-headline mb2">
+            {get(fields, 'secondTitle', '')}
+          </h2>
+          <p className="copy">{get(fields, 'secondText', '')}</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ImageDoubleText;
