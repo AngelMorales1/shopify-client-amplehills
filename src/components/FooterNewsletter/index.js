c7c959583e2d0943405bdece271d38804bf53cc4
Joshie Fishbein <joshie@sanctuary.computer>
2018-07-05 18:24:44 -0400

    convert FooterNewsletter to FSC

--- src/components/FooterNewsletter/index.js
+++ src/components/FooterNewsletter/index.js
@@ -1,64 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import cx from 'classnames';
 import styles from './FooterNewsletter.scss';
 import RoutesWithoutNewsletter from 'constants/RoutesWithoutNewsletter';
 import { Image, Button, TextField } from 'components/base';
 
-class FooterNewsletter extends Component {
-  routeOmitsNewsletter() {
+const FooterNewsletter = () => {
+  const routeOmitsNewsletter = () => {
     return RoutesWithoutNewsletter.includes(this.props.pathname);
-  }
+  };
 
-  render() {
-    if (this.routeOmitsNewsletter()) return null;
-    return (
+  if (routeOmitsNewsletter()) return null;
+  return (
+    <div
+      className={cx(
+        styles['FooterNewsletter'],
+        'my4 py4 px3 flex items-center'
+      )}
+    >
       <div
         className={cx(
-          styles['FooterNewsletter'],
-          'my4 py4 px3 flex items-center'
+          styles['FooterNewsletter__container'],
+          'flex items-center'
         )}
       >
-        <div
+        <p
           className={cx(
-            styles['FooterNewsletter__container'],
-            'flex items-center'
+            styles['FooterNewsletter__text'],
+            'my2 text-peach center'
           )}
         >
-          <p
-            className={cx(
-              styles['FooterNewsletter__text'],
-              'my2 text-peach center'
-            )}
-          >
-            Sign up for the latest news from us!
-          </p>
-          <Image
-            className="my2 xs-hide sm-hide icon"
-            alt="Decorative arrow"
-            src="/assets/images/arrow-straight-right.svg"
-          />
-        </div>
-        <div
-          className={cx(
-            styles['FooterNewsletter__container'],
-            'flex items-center'
-          )}
-        >
-          <TextField
-            className={cx(styles['FooterNewsletter__text-field'], 'my2 pb1')}
-            placeholder="Enter your email address"
-            variant="madison-blue-outline-round"
-          />
-          <Button
-            className="my2 px3"
-            label="Sign Up"
-            variant="primary"
-            color="madison-blue"
-          />
-        </div>
+          Sign up for the latest news from us!
+        </p>
+        <Image
+          className="my2 xs-hide sm-hide icon"
+          alt="Decorative arrow"
+          src="/assets/images/arrow-straight-right.svg"
+        />
       </div>
-    );
-  }
-}
+      <div
+        className={cx(
+          styles['FooterNewsletter__container'],
+          'flex items-center'
+        )}
+      >
+        <TextField
+          className={cx(styles['FooterNewsletter__text-field'], 'my2 pb1')}
+          placeholder="Enter your email address"
+          variant="madison-blue-outline-round"
+        />
+        <Button
+          className="my2 px3"
+          label="Sign Up"
+          variant="primary"
+          color="madison-blue"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default FooterNewsletter;
