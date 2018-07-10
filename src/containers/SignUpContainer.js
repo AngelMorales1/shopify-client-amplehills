f7b9f03df21bc9298f82c3d0703f1a8c5c7553de
Joshie Fishbein <joshie@sanctuary.computer>
2018-07-10 17:06:26 -0400

    add sign up form and redux

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -2,17 +2,27 @@ import ContainerBase from 'lib/ContainerBase';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import { signUpCustomer } from 'state/actions/customerActions';
+import customer from 'state/selectors/customer';
+
 class SignUpContainer extends ContainerBase {
   view = import('views/SignUpView');
 }
 
 const mapStateToProps = state => {
-  return {};
+  return {
+    customer: customer(state)
+  };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    actions: bindActionCreators({}, dispatch)
+    actions: bindActionCreators(
+      {
+        signUpCustomer
+      },
+      dispatch
+    )
   };
 };
 
--- src/state/actions/customerActions.js
+++ src/state/actions/customerActions.js
@@ -1,17 +1,31 @@
 import { client as Apollo } from 'lib/Apollo';
 import {
+  customerCreate,
   customerAccessTokenCreate,
   customerFetch
 } from 'state/graphql/customer';
 import get from 'utils/get';
 
+export const SIGN_UP_CUSTOMER = 'SIGN_UP_CUSTOMER';
+export const signUpCustomer = input => dispatch => {
+  return dispatch({
+    type: SIGN_UP_CUSTOMER,
+    payload: Apollo.mutate({
+      mutation: customerCreate,
+      variables: { input }
+    }).then(customer => {
+      console.log(customer);
+    })
+  });
+};
+
 export const SIGN_IN_CUSTOMER = 'SIGN_IN_CUSTOMER';
-export const signInCustomer = payload => dispatch => {
+export const signInCustomer = input => dispatch => {
   return dispatch({
     type: SIGN_IN_CUSTOMER,
     payload: Apollo.mutate({
       mutation: customerAccessTokenCreate,
-      variables: { input: payload }
+      variables: { input }
     }).then(customerAccessToken => {
       const accessToken = get(
         customerAccessToken,
