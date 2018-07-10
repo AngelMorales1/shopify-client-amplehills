3803089a5acc560b85add13e69944a98e65b7271
Joshie Fishbein <joshie@sanctuary.computer>
2018-07-10 17:17:24 -0400

    add successful sign-up forwarding to sign-in

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -5,13 +5,24 @@ import { bindActionCreators } from 'redux';
 import { signUpCustomer } from 'state/actions/customerActions';
 import customer from 'state/selectors/customer';
 
+import { PENDING, FULFILLED } from 'constants/Status';
+import get from 'utils/get';
+
 class SignUpContainer extends ContainerBase {
+  componentDidUpdate(prevProps) {
+    const customerSignedUp =
+      prevProps.customerSigningUp === PENDING &&
+      this.props.customerSigningUp === FULFILLED;
+    if (customerSignedUp) this.props.history.push(`/sign-in?new-account=true`);
+  }
+
   view = import('views/SignUpView');
 }
 
 const mapStateToProps = state => {
   return {
-    customer: customer(state)
+    customer: customer(state),
+    customerSigningUp: get(state, 'status.customerSigningUp', false)
   };
 };
 
