db8f07a4567276487fd9c10707c12b68fae8f94c
Joshie Fishbein <joshie@sanctuary.computer>
2018-07-16 19:07:44 -0400

    conver OurPledge to FSC

--- src/components/OurPledge/index.js
+++ src/components/OurPledge/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import get from 'utils/get';
@@ -8,85 +8,74 @@ import styles from './OurPledge.scss';
 import { Image, Button } from 'components/base';
 import OurPledgeOverlay from 'components/OurPledgeOverlay';
 
-class OurPledge extends Component {
-  openOurPledgeOverlay = () => {
-    this.props.actions.openOurPledge();
-  };
+const OurPledge = ({
+  actions,
+  ourPledgeOverlayIsOpen,
+  overlayContentImage,
+  shippingInformation,
+  shippingPledge,
+  calloutImage
+}) => {
+  const calloutImageUrl = get(calloutImage, 'fields.file.url', '');
 
-  closeOurPledgeOverlay = () => {
-    this.props.actions.closeOurPledge();
-  };
-
-  render() {
-    const {
-      ourPledgeOverlayIsOpen,
-      overlayContentImage,
-      shippingInformation,
-      shippingPledge,
-      calloutImage
-    } = this.props;
-
-    const calloutImageUrl = get(calloutImage, 'fields.file.url', '');
-
-    return (
+  return (
+    <div
+      className={cx(
+        'col-12 mx-auto mt3 flex flex-column items-center',
+        styles['OurPledge']
+      )}
+    >
+      <Image
+        alt="Our pledge icon"
+        src={calloutImageUrl}
+        className={cx('icon z-1', styles['OurPledge__icon'])}
+      />
       <div
         className={cx(
-          'col-12 mx-auto mt3 flex flex-column items-center',
-          styles['OurPledge']
+          'bg-varden flex items-center w100',
+          styles['OurPledge__content-container']
         )}
       >
-        <Image
-          alt="Our pledge icon"
-          src={calloutImageUrl}
-          className={cx('icon z-1', styles['OurPledge__icon'])}
-        />
         <div
           className={cx(
-            'bg-varden flex items-center w100',
-            styles['OurPledge__content-container']
+            styles['OurPledge__text-content-container'],
+            'flex col-8'
           )}
         >
-          <div
+          <p className="col md-col-6 px2 block-subheadline carter nowrap">
+            Our Pledge
+          </p>
+          <p
             className={cx(
-              styles['OurPledge__text-content-container'],
-              'flex col-8'
+              styles['OurPledge__text-description'],
+              'col col-12 md-col-6 px2 flex uppercase semi-bold flex'
             )}
           >
-            <p className="col md-col-6 px2 block-subheadline carter nowrap">
-              Our Pledge
-            </p>
-            <p
-              className={cx(
-                styles['OurPledge__text-description'],
-                'col col-12 md-col-6 px2 flex uppercase semi-bold flex'
-              )}
-            >
-              Ice cream arrives fresh delicious, and frozen
-            </p>
-          </div>
-          <div className="col col-4 px2 right-align flex justify-end">
-            <Button
-              variant="style-none"
-              onClick={this.openOurPledgeOverlay}
-              label="More Info"
-              className={cx(
-                'uppercase info-text-big bold nowrap text-madison-blue',
-                styles['OurPledge__more-info']
-              )}
-            />
-          </div>
+            Ice cream arrives fresh delicious, and frozen
+          </p>
         </div>
-        <OurPledgeOverlay
-          overlayContentImage={overlayContentImage}
-          shippingInformation={shippingInformation}
-          shippingPledge={shippingPledge}
-          closeOurPledgeOverlay={this.closeOurPledgeOverlay}
-          ourPledgeOverlayIsOpen={ourPledgeOverlayIsOpen}
-        />
+        <div className="col col-4 px2 right-align flex justify-end">
+          <Button
+            variant="style-none"
+            onClick={actions.openOurPledge}
+            label="More Info"
+            className={cx(
+              'uppercase info-text-big bold nowrap text-madison-blue',
+              styles['OurPledge__more-info']
+            )}
+          />
+        </div>
       </div>
-    );
-  }
-}
+      <OurPledgeOverlay
+        overlayContentImage={overlayContentImage}
+        shippingInformation={shippingInformation}
+        shippingPledge={shippingPledge}
+        closeOurPledgeOverlay={actions.closeOurPledge}
+        ourPledgeOverlayIsOpen={ourPledgeOverlayIsOpen}
+      />
+    </div>
+  );
+};
 
 export default OurPledge;
 
