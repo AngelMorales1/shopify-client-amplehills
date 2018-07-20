00b2b419e2adf85db65b69f3211cabddda80e10b
Joshie Fishbein <joshie@sanctuary.computer>
2018-07-20 13:29:02 -0400

    add ContactUsForm as a constant

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import { Radio, TextField, Button } from 'components/base';
+import ContactUsForm from 'constants/forms/ContactUs';
+import { Dropdown, Radio, TextField, Button } from 'components/base';
 
 import cx from 'classnames';
 import styles from './ContactUs.scss';
 
 class ContactUs extends Component {
-  constructor() {
-    super(...arguments);
+  state = {
+    selectedAddress: ''
+  };
 
-    this.state = {
-      checkedRadioField: ''
-    };
-  }
-
-  handleRadioClick = field => {
-    this.setState({ checkedRadioField: field });
+  handleChangeAddress = selectedAddress => {
+    this.setState({ selectedAddress });
   };
 
   render() {
@@ -25,66 +22,58 @@ class ContactUs extends Component {
           'flex flex-column justify-around items-center'
         )}
       >
-        <h2 className="block-headline m2 center">Contact us</h2>
-        <p className="m2 center">What can we help you with?</p>
-        <form className="flex m3">
-          <Radio
-            checked={this.state.checkedRadioField === 'ordering'}
-            onClick={() => this.handleRadioClick('ordering')}
-            className="mx3"
-            label="Ordering"
-          />
-          <Radio
-            checked={this.state.checkedRadioField === 'shipping'}
-            onClick={() => this.handleRadioClick('shipping')}
-            className="mx3"
-            label="Shipping"
-          />
-          <Radio
-            checked={this.state.checkedRadioField === 'other'}
-            onClick={() => this.handleRadioClick('other')}
-            className="mx3"
-            label="Other"
-          />
+        <h2 className="block-headline my2 px2 center">Contact us</h2>
+        <p className="my2 px2 center">What can we help you with?</p>
+        <form className="flex flex-wrap justify-center text-container-width">
+          <div className="flex flex-wrap justify-center px2 my2">
+            {Object.values(ContactUsForm).map(field => (
+              <Radio
+                checked={this.state.selectedAddress === field.email}
+                onClick={() => this.setState({ selectedAddress: field.email })}
+                className="mx2 my1 small"
+                label={field.label}
+              />
+            ))}
+          </div>
+          <div className="w100 flex flex-column">
+            <TextField
+              className="m1"
+              variant="light-gray"
+              type="text"
+              placeholder="Name"
+            />
+            <TextField
+              className="m1"
+              variant="light-gray"
+              type="email"
+              placeholder="Email Address"
+            />
+            <TextField
+              className="m1"
+              variant="light-gray"
+              type="tel"
+              placeholder="Phone Number"
+            />
+            <TextField
+              className="m1"
+              variant="light-gray-tall"
+              placeholder="Message"
+              multiLine={true}
+            />
+          </div>
+          <div
+            className={cx(
+              styles['ContactUs__button-container'],
+              'w100 flex text-container-width'
+            )}
+          >
+            <Button
+              label="Send Us a Message"
+              color="madison-blue"
+              className="m1"
+            />
+          </div>
         </form>
-        <div className="flex flex-column m3 wh100 text-container-width">
-          <TextField
-            className="m1"
-            variant="light-gray"
-            type="text"
-            placeholder="Name"
-          />
-          <TextField
-            className="m1"
-            variant="light-gray"
-            type="email"
-            placeholder="Email Address"
-          />
-          <TextField
-            className="m1"
-            variant="light-gray"
-            type="tel"
-            placeholder="Phone Number"
-          />
-          <TextField
-            className="m1"
-            variant="light-gray-tall"
-            placeholder="Message"
-            multiLine={true}
-          />
-        </div>
-        <div
-          className={cx(
-            styles['ContactUs__button-container'],
-            'w100 flex text-container-width'
-          )}
-        >
-          <Button
-            label="Send Us a Message"
-            color="madison-blue"
-            className="m1"
-          />
-        </div>
       </div>
     );
   }
---
+++ src/constants/forms/ContactUs.js
@@ -0,0 +1,22 @@
+export default {
+  GENERAL: {
+    label: 'General Info',
+    email: 'info@amplehills.com'
+  },
+  PRESS: {
+    label: 'Press',
+    email: 'press@amplehills.com'
+  },
+  PARTIES: {
+    label: 'Parties',
+    email: 'parties@amplehills.com'
+  },
+  EVENTS: {
+    label: 'Off-Site Events',
+    email: 'events@amplehills.com'
+  },
+  CONCERNS: {
+    label: 'Comments & Concerns',
+    email: 'comments@amplehills.com'
+  }
+};
