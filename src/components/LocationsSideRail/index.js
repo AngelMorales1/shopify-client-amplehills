f76eab624b57c1ef6628281a29d5ff4c83000c8c
sohee Lim <limsohee1002@gmail.com>
2018-07-30 16:49:45 -0400

    Add font style

--- src/components/LocationsSideRail/index.js
+++ src/components/LocationsSideRail/index.js
@@ -22,46 +22,76 @@ const LocationsSideRail = props => {
   };
 
   return (
-    <div className={cx(styles['LocationsSideRail'], 'overflow-scroll')}>
-      {locations.map((location, index) => {
-        const locationOpenHours = getTodayOpenHours(location);
+    <div
+      className={cx(
+        styles['LocationsSideRail'],
+        'overflow-scroll bg-goldenrod'
+      )}
+    >
+      <div className="mt3 mb4 col-10 mx-auto">
+        {locations.map((location, index) => {
+          const locationOpenHours = getTodayOpenHours(location);
+          const imageUrl = get(location, 'image.fields.file.url', '');
 
-        return (
-          <div key={`${index}-${location.id}`} className="col-10 mx-auto">
-            <div>
-              <Image
-                src={get(location, 'image.fields.file.url', '')}
-                alt={`${location.title} image`}
+          return (
+            <div
+              key={`${index}-${location.id}`}
+              className={cx(
+                styles['LocationsSideRail__card-container'],
+                'bg-white my3'
+              )}
+            >
+              <div
+                style={
+                  imageUrl.length > 0
+                    ? {
+                        background: `url(${imageUrl}) no-repeat center`,
+                        backgroundSize: 'cover'
+                      }
+                    : { backgroundColor: '#1a6db6' }
+                }
+                className={cx(styles['LocationsSideRail__card-image'])}
               />
-            </div>
-            <div>
-              <h2>{location.title}</h2>
-              <div>
+              <div className="p2">
+                <h2 className="description-title carter">{location.title}</h2>
                 <div>
-                  <span>{location.address1}</span>
-                  <span>{location.address2}</span>
-                  <span>{`${location.city}, ${location.state} ${
-                    location.zip
-                  }`}</span>
+                  <div className="flex flex-column">
+                    <span className="tout">{location.address1}</span>
+                    <span className="tout">{location.address2}</span>
+                    <span className="tout">{`${location.city}, ${
+                      location.state
+                    } ${location.zip}`}</span>
+                    <span className="tout">{location.phone}</span>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex flex-row justify-between mt2">
+                    {locationOpenHours.length ? (
+                      <div className="flex flex-column">
+                        <span className="block-subheadline bold">
+                          Open today
+                        </span>
+                        <span className="tout">
+                          {location[locationOpenHours[0]]}
+                        </span>
+                      </div>
+                    ) : (
+                      <span className="block-subheadline bold">
+                        Closed today
+                      </span>
+                    )}
+                    {location.delivery ? (
+                      <div className="tag uppercase bg-madison-blue info-text-big text-white inline-block">
+                        Delivery
+                      </div>
+                    ) : null}
+                  </div>
                 </div>
               </div>
-              <div>
-                <div>
-                  {locationOpenHours.length ? (
-                    <div>
-                      <span>Open today</span>
-                      <span>{location[locationOpenHours[0]]}</span>
-                    </div>
-                  ) : (
-                    <span>Closed today</span>
-                  )}
-                  {location.delivery ? <div>Delivery</div> : null}
-                </div>
-              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
