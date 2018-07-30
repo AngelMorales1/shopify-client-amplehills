5884eb5543ba4879cf2fb4a9f52f71bd36fa16d0
sohee Lim <limsohee1002@gmail.com>
2018-07-30 09:08:59 -0400

    Fix profile

--- src/components/ProfileOrders/index.js
+++ src/components/ProfileOrders/index.js
@@ -26,55 +26,71 @@ const ProfileOrders = ({ actions, checkout, orders, products }) => {
     <div className="my3">
       <h2 className="carter sub-title mb3">Order History</h2>
       <div>
-        {orders.map(order => (
-          <div
-            key={order.id}
-            className="card card--light-gray-border px3 pt3 pb2 my2"
-          >
-            <p className="bold text-peach mb3">
-              {moment(order.date).format('MMMM D, YYYY')}
-            </p>
-            {order.items.map((item, i) => (
-              <div
-                key={`${item.title} ${i}`}
-                className="mb2 flex flex-wrap justify-between"
-              >
-                <span className="line-item-title mr2">{item.title}</span>
-                <span className="line-item-title">${item.price}</span>
-                {item.subItems.length ? (
-                  <div className="w100">
-                    <ul className="mt2 mb1">
-                      {item.subItems.map((subItem, i) => (
-                        <li
-                          className="sub-line-item small"
-                          key={`${subItem.handle} ${i}`}
-                        >{`${subItem.quantity}x ${
-                          products[subItem.handle].title
-                        }`}</li>
-                      ))}
-                    </ul>
-                  </div>
-                ) : null}
+        {orders.length ? (
+          orders.map(order => (
+            <div
+              key={order.id}
+              className="card card--light-gray-border px3 pt3 pb2 my2"
+            >
+              <p className="bold text-peach mb3">
+                {moment(order.date).format('MMMM D, YYYY')}
+              </p>
+              {order.items.map((item, i) => (
+                <div
+                  key={`${item.title} ${i}`}
+                  className="mb2 flex flex-wrap justify-between"
+                >
+                  <span className="line-item-title mr2">{item.title}</span>
+                  <span className="line-item-title">${item.price}</span>
+                  {item.subItems.length ? (
+                    <div className="w100">
+                      <ul className="mt2 mb1">
+                        {item.subItems.map((subItem, i) => (
+                          <li
+                            className="sub-line-item small"
+                            key={`${subItem.handle} ${i}`}
+                          >{`${subItem.quantity}x ${
+                            products[subItem.handle].title
+                          }`}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  ) : null}
+                </div>
+              ))}
+              <div className="mt4 flex flex-wrap">
+                <Button
+                  className="tag mb2 mr2 bg-peach text-white overflow-auto"
+                  variant="secondary"
+                  label="View Receipt"
+                  shadow={true}
+                  to={order.receipt}
+                />
+                <Button
+                  className="tag mb2 bg-peach text-white"
+                  variant="secondary"
+                  shadow={true}
+                  label="Re-order"
+                  onClick={() => handleReorder(order)}
+                />
               </div>
-            ))}
-            <div className="mt4 flex flex-wrap">
-              <Button
-                className="tag mb2 mr2 bg-peach text-white w-auto"
-                variant="secondary"
-                label="View Receipt"
-                shadow={true}
-                to={order.receipt}
-              />
-              <Button
-                className="tag mb2 bg-peach text-white"
-                variant="secondary"
-                shadow={true}
-                label="Re-order"
-                onClick={() => handleReorder(order)}
-              />
             </div>
+          ))
+        ) : (
+          <div className="card card--light-gray-border px3 pt3 pb2 my2 flex flex-column items-center">
+            <strong className="bold block mb2">
+              Your order history will live here.
+            </strong>
+            <p>What will your first purchase be?</p>
+            <Button
+              className="small mt4"
+              variant="primary-small"
+              color="peach"
+              label="Let&apos;s explore!"
+              to={'/products'}
+            />
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
