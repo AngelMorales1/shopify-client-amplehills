00a071d5192496ffb463c6fc8dfece3edcf9f0c7
Joshie Fishbein <joshie@sanctuary.computer>
2018-07-30 14:39:22 -0400

    add redirect to mickey PDP from homepage

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
 class HomeView extends Component {
   render() {
     const { model } = this.props;
     if (model.isError) return <h1>Error</h1>;
 
-    return <div className="Home">Welcome Home!</div>;
+    return <Redirect to="/products/mickey-celebration" />;
   }
 }
 
