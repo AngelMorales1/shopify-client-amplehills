769ad3b38f28f7cd3c720e2172623b786d3c566b
sohee Lim <limsohee1002@gmail.com>
2018-07-31 16:47:14 -0400

    Fix align

--- src/components/OurPledge/index.js
+++ src/components/OurPledge/index.js
@@ -39,16 +39,16 @@ const OurPledge = ({
         <div
           className={cx(
             styles['OurPledge__text-content-container'],
-            'flex col-9'
+            'flex col-8 md-col-9'
           )}
         >
-          <p className="col md-col-5 px2 block-subheadline carter nowrap">
+          <p className="col col-12 md-col-4 px2 block-subheadline carter nowrap">
             Our Pledge
           </p>
           <p
             className={cx(
               styles['OurPledge__text-description'],
-              'tout col col-12 md-col-7 px2 flex uppercase semi-bold flex'
+              'tout col col-12 md-col-8 px2 flex uppercase semi-bold flex'
             )}
           >
             Ice cream arrives fresh delicious, and frozen
