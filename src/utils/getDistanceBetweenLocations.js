b6e44f8127a1b664a484ae6fecd08f0acd4702ef
sohee Lim <limsohee1002@gmail.com>
2018-08-01 12:11:13 -0400

    render by distance

--- src/utils/getDistanceBetweenLocations.js
+++ src/utils/getDistanceBetweenLocations.js
@@ -1,33 +1,9 @@
-
 import distance from '@turf/distance';
-
-// const straightAngle = 180;
-// const minutesInADegree = 60;
-// const nuticalMile = 1.1515;
+import { point } from '@turf/helpers';
 
 export const getDistanceBetweenLocations = (storeLat, storeLon, currentLat, currentLon) => {
-  // const radlat1 = (Math.PI * lat1) / straightAngle;
-  // const radlat2 = (Math.PI * lat2) / straightAngle;
-  // const theta = lon1 - lon2;
-  // const radtheta = (Math.PI * theta) / straightAngle;
-  // let dist =
-  //   Math.sin(radlat1) * Math.sin(radlat2) +
-  //   Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-
-  // if (dist > 1) {
-  //   dist = 1;
-  // }
-
-  // dist = Math.acos(dist);
-  // dist = (dist * straightAngle) / Math.PI;
-  // dist = dist * minutesInADegree * nuticalMile;
-  
-  // const roundedDistance = Math.round(dist * 10) / 10;
-
-  // return roundedDistance;
-
-  const from = [currentLat, currentLon];
-  const to = [storeLat, storeLon];
+  const from = point([currentLon, currentLat]);
+  const to = point([storeLon, storeLat]);
   const options = {units: 'miles'};
 
   const distanceBetweenLocations = distance(from, to, options);
