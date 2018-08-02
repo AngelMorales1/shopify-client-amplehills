607cc6802e1a4b46529e53b102dd801b94345edc
Joshie Fishbein <joshie@sanctuary.computer>
2018-08-02 12:09:40 -0400

    save mocks into mock folder to prevent data repition

---
+++ src/tests/state/mocks/selected/locations.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: '0001',
+    title: 'Location 1',
+    image: 'path-to-image.jpg',
+    seasonalImage: 'path-to-image.jpg',
+    address1: '110 Bowery',
+    address2: 'Fl. 4',
+    city: 'New York',
+    region: 'Manhattan',
+    state: 'NY',
+    zip: '10013',
+    coordinates: {
+      lon: 80.123,
+      lat: 40.789
+    },
+    phone: '8881234567',
+    seasonal: false,
+    delivery: false,
+    hours: {
+      sunday: 'Noon–12am',
+      monday: 'Noon–11pm',
+      tuesday: 'Noon–11pm',
+      wednesday: 'Noon–11pm',
+      thursday: 'Noon–11pm',
+      friday: 'Noon–11pm',
+      saturday: 'Noon–11pm'
+    },
+    sortedHours: [{ Sun: 'Noon–12am' }, { 'Mon–Sat': 'Noon–11pm' }]
+  }
+];
---
+++ src/tests/state/mocks/unselected/locations.js
@@ -0,0 +1,46 @@
+export default {
+  items: [
+    {
+      sys: {
+        id: '0001'
+      },
+      fields: {
+        title: 'Location 1',
+        image: {
+          fields: {
+            file: {
+              url: 'path-to-image.jpg'
+            }
+          }
+        },
+        seasonalImage: {
+          fields: {
+            file: {
+              url: 'path-to-image.jpg'
+            }
+          }
+        },
+        address1: '110 Bowery',
+        address2: 'Fl. 4',
+        city: 'New York',
+        region: 'Manhattan',
+        state: 'NY',
+        zip: '10013',
+        location: {
+          lon: 80.123,
+          lat: 40.789
+        },
+        phone: '8881234567',
+        seasonal: false,
+        delivery: false,
+        sunday: 'Noon–12am',
+        monday: 'Noon–11pm',
+        tuesday: 'Noon–11pm',
+        wednesday: 'Noon–11pm',
+        thursday: 'Noon–11pm',
+        friday: 'Noon–11pm',
+        saturday: 'Noon–11pm'
+      }
+    }
+  ]
+};
