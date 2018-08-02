a8b258d44541e9b9ee76733229e2b8b6c128565c
Joshie Fishbein <joshie@sanctuary.computer>
2018-08-02 12:41:44 -0400

    run selectors dependencies in depedent selector tests, add locationsGeoJSON selector test

---
+++ src/tests/state/selectors/locationGeoJSON.test.js
@@ -0,0 +1,11 @@
+import unselectedLocations from 'tests/state/mocks/unselected/locations';
+import selectedLocationGeoJSON from 'tests/state/mocks/selected/locationGeoJSON';
+import locations from 'state/selectors/locations';
+import locationGeoJSON from 'state/selectors/locationGeoJSON';
+
+it('locationGeoJSON selector works as intended', () => {
+  const selectedLocations = locations.resultFunc(unselectedLocations);
+  expect(locationGeoJSON.resultFunc(selectedLocations)).toEqual(
+    selectedLocationGeoJSON
+  );
+});
