e5b88cf7c0d15001e8e8439ee416a539a979c533
sohee Lim <limsohee1002@gmail.com>
2018-08-02 12:26:26 -0400

    Add BlockSwitch test

---
+++ src/tests/components/BlockSwitch.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import BlockSwitch from 'components/BlockSwitch';
+import contentful from './data/BlockSwitch.contentful.js';
+
+it('renders without data', () => {
+  const component = shallow(<BlockSwitch />);
+
+  expect(component).toMatchSnapshot();
+});
+
+it('renders with data', () => {
+  const component = shallow(<BlockSwitch data={contentful} z={1} />);
+
+  expect(component).toMatchSnapshot();
+});
