da86131be70a429747eb85a8d6a866662bc445e5
Joshie Fishbein <joshie@sanctuary.computer>
2018-08-02 11:58:41 -0400

    add locations selector test

--- src/state/selectors/ourPledge.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import get from 'utils/get';
-import products from 'state/selectors/products';
-
-export default createSelector(
-  state => products(state),
-  state => get(state, 'product.handle', ''),
-  (products, handle) => products[handle]
-);
