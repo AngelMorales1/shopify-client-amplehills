6c0542130160b9069d7d062d8b5afc3f1a756bf8
sohee Lim <limsohee1002@gmail.com>
2018-08-03 14:56:16 -0400

    Add LocationsMap test

---
+++ src/tests/components/LocationsMap.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LocationsMap from 'components/LocationsMap';
+
+it('renders without data', () => {
+  const component = shallow(<LocationsMap />);
+
+  expect(component).toMatchSnapshot();
+});
