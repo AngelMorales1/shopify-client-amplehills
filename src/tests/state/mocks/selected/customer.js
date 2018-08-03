3584f6288c912890f7abb91fcb437969297524cf
sohee Lim <limsohee1002@gmail.com>
2018-08-03 17:33:37 -0400

    Remove addresses

--- src/tests/state/mocks/selected/customer.js
+++ src/tests/state/mocks/selected/customer.js
@@ -4,21 +4,6 @@ export default {
   firstName: 'Test',
   lastName: 'User',
   phone: '8881234567',
-  addresses: [
-    {
-      firstName: 'Test',
-      lastName: 'User',
-      address1: '110 Bowery',
-      address2: 'Fl. 4',
-      city: 'New York',
-      company: 'Sanctuary Computer',
-      province: 'NY',
-      zip: '10013',
-      country: 'United States of America',
-      countryCode: 'US',
-      phone: '8881234567'
-    }
-  ],
   accessToken: '9999',
   orders: [
     {
