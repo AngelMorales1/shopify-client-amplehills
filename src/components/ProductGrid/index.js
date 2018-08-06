dc1b2dddcc2e07f6ae21b8e3dd484aeb0e164a87
sohee Lim <limsohee1002@gmail.com>
2018-08-06 12:44:18 -0400

    Fix products.map in ProductGrid

--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -7,7 +7,7 @@ const ProductGrid = ({ products }) => {
   return (
     <div className="ProductGrid">
       <div className="flex flex-wrap container-width mx-auto px2">
-        {Object.values(products).map(product => (
+        {products.map(product => (
           <ProductGridCard key={product.id} product={product} />
         ))}
       </div>
