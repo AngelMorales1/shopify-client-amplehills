a65fc8fb48c4c73f3e3301216014f549527ecda4
sohee Lim <limsohee1002@gmail.com>
2018-08-10 14:38:14 -0400

    fix selector

--- src/state/selectors/alertIsActive.js
+++ src/state/selectors/alertIsActive.js
@@ -2,6 +2,6 @@ import { createSelector } from 'reselect';
 import get from 'utils/get';
 
 export default createSelector(
-  state => get(state, 'applicationUI.globalSettings.items[0].fields', {}),
-  fields => !!fields.alert
+  state => get(state, 'applicationUI.globalSettings.items[0].fields.alert', {}),
+  alert => !!get(alert, 'fields', {}).alertCopy
 );
