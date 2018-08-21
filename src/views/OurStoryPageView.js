73532b66874ecd31e78e62576bf023a57302b84c
sohee Lim <limsohee1002@gmail.com>
2018-08-21 17:28:15 -0400

    Add contentful image util

--- src/views/OurStoryPageView.js
+++ src/views/OurStoryPageView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import get from 'utils/get';
+import contentfulImgUtil from 'utils/contentfulImgUtil';
 
 import { Image } from 'components/base';
 import BlockSwitch from 'components/BlockSwitch';
@@ -18,7 +19,11 @@ class OurStoryPageView extends Component {
           <Image
             className="col-8 md-col-6 mt4"
             alt={`${title} image`}
-            src={get(image, 'fields.file.url', '')}
+            src={contentfulImgUtil(
+              get(image, 'fields.file.url', ''),
+              '1400',
+              'png'
+            )}
           />
         </div>
         {blocks &&
