54a3b4bf65b1ce231daa47f790aa30ba92a1593b
sohee Lim <limsohee1002@gmail.com>
2018-08-21 12:36:00 -0400

    Get data from contentful

---
+++ src/containers/OurStoryPageContainer.js
@@ -0,0 +1,47 @@
+import ContainerBase from 'lib/ContainerBase';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { getOurStoryPage } from 'state/actions/ourStoryPageActions';
+
+import get from 'utils/get';
+
+class OurStoryPageContainer extends ContainerBase {
+  view = import('views/OurStoryPageView');
+
+  model = () => {
+    const { getOurStoryPage } = this.props.actions;
+
+    return getOurStoryPage();
+  };
+}
+
+const mapStateToProps = (state, props) => {
+  const fields = get(
+    state,
+    'ourStoragePage.ourStoragePage.items[0].fields',
+    {}
+  );
+
+  return {
+    image: get(fields, 'image', {}),
+    title: get(fields, 'title', ''),
+    block: get(fields, 'contentBlock', [])
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(
+      {
+        getOurStoryPage
+      },
+      dispatch
+    )
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OurStoryPageContainer);
