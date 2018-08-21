13022c2b3936609e2d1131ebd5c111515376d09f
sohee Lim <limsohee1002@gmail.com>
2018-08-21 09:23:01 -0400

    Add style to press page

--- src/components/PressGrid/index.js
+++ src/components/PressGrid/index.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'utils/get';
+import cx from 'classnames';
 
-import { Image } from 'components/base';
+import { Image, Button } from 'components/base';
+import styles from './PressGrid.scss';
+import imageModel from 'models/imageModel';
 
 const PressGrid = ({ pressBlock }) => {
+  const fields = get(pressBlock, 'fields', {});
+
   return (
-    <div className="border flex flex-column justify-center items-center">
+    <div
+      className={cx(
+        styles['PressGrid'],
+        'm2 pt4 pb3 px3 flex flex-column justify-center items-center'
+      )}
+    >
       <Image
-        className="col-1"
-        src={get(pressBlock, 'fields.image.fields.file.url', '')}
-        alt={`${get(pressBlock, 'fields.title', '')} logo`}
+        className={cx(styles['PressGrid__logo'])}
+        src={fields.image.fields.file.url}
+        alt={`${fields.title} logo`}
       />
-      <span>{`"${get(pressBlock, 'fields.quote', '')}"`}</span>
-      <a
-        href={get(pressBlock, 'fields.linkUrl', '')}
-        target="_blank"
-        rel="noopener"
-      >
-        <div className="bg-peach text-white inline-block mt1 nowrap tag">
-          Read about it
-        </div>
-      </a>
+      <span
+        className={cx(
+          styles['PressGrid__quote'],
+          'carter text-peach center py3 mb4'
+        )}
+      >{`"${fields.quote}"`}</span>
+      <Button
+        className={cx(styles['PressGrid__button'], 'uppercase')}
+        to={fields.linkUrl}
+        label="Read about it"
+        variant="primary-small"
+        color="peach"
+      />
     </div>
   );
 };
 
 export default PressGrid;
 
-PressGrid.propTypes = {};
+PressGrid.propTypes = {
+  pressBlock: PropTypes.shape({
+    fields: PropTypes.shape({
+      image: imageModel.propTypes,
+      linkUrl: PropTypes.string,
+      quote: PropTypes.string,
+      title: PropTypes.string
+    })
+  })
+};
 
-PressGrid.defaultProps = {};
+PressGrid.defaultProps = {
+  pressBlock: {
+    fields: {
+      image: imageModel.default,
+      linkUrl: '',
+      quote: '',
+      title: ''
+    }
+  }
+};
