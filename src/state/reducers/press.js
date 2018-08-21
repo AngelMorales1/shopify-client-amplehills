9bb68e2b51531461b45a0dc3db225074960a49c5
sohee Lim <limsohee1002@gmail.com>
2018-08-21 14:26:20 -0400

    change data source title

--- src/state/reducers/press.js
+++ src/state/reducers/press.js
@@ -10,7 +10,7 @@ export default (state = initialState, action) => {
     case `${GET_PRESS_ITEMS}_FULFILLED`:
       return {
         ...state,
-        pressItems: action.payload
+        press: action.payload
       };
     default:
       return state;
