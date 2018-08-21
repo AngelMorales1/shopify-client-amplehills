62cf929150fc6497ff7ad89c63b0aef0a4a6305c
sohee Lim <limsohee1002@gmail.com>
2018-08-21 17:13:17 -0400

    Change name from Page to Data

--- src/state/actions/ourStoryPageActions.js
+++ src/state/actions/ourStoryPageActions.js
@@ -1,9 +1,9 @@
 import Data from 'lib/Data';
 
-export const GET_OUR_STORY_PAGE = 'GET_OUR_STORY_PAGE';
+export const GET_OUR_STORY_DATA = 'GET_OUR_STORY_DATA';
 export const getOurStoryPage = payload => dispatch => {
   return dispatch({
-    type: GET_OUR_STORY_PAGE,
+    type: GET_OUR_STORY_DATA,
     payload: Data.getEntries({
       content_type: 'ourStoryPage'
     })
