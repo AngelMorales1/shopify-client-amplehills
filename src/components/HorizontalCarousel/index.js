52cd1562bf154d989c7c296dc2c55df52b906e44
sohee Lim <limsohee1002@gmail.com>
2018-08-22 17:30:42 -0400

    Add HorizontalCarousel block

---
+++ src/components/HorizontalCarousel/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import get from 'utils/get';
+import contentfulImgUtil from 'utils/contentfulImgUtil';
+import cx from 'classnames';
+
+import { Image } from 'components/base';
+
+class HorizontalCarousel extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default HorizontalCarousel;
