94da000cd57cf8727bd5d8be5f0963417f37bfcf
sohee Lim <limsohee1002@gmail.com>
2018-08-22 15:35:38 -0400

    Add additional ImageText block style

--- src/components/ImageText/index.js
+++ src/components/ImageText/index.js
@@ -1,62 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
 import cx from 'classnames';
 import get from 'utils/get';
 import imageModel from 'models/imageModel';
 import contentfulImgUtil from 'utils/contentfulImgUtil';
+import Global from 'constants/Global';
 
 import styles from './ImageText.scss';
 import { Image } from 'components/base';
 
-const ImageText = ({ block, z }) => {
-  const fields = get(block, 'fields', {});
-  const colorClass = `ImageText--${get(fields, 'backgroundColor', 'pink')}`;
-  const position = get(fields, 'imagePosition', 0);
-  const isReverseArrangement = get(fields, 'isReverseArrangement', false);
+class ImageText extends Component {
+  state = {
+    currentBreakpoint: Global.breakpoints.small.label
+  };
 
-  return (
-    <div
-      style={{ zIndex: z }}
-      className={cx(
-        styles['ImageText'],
-        styles[colorClass],
-        'flex flex-row justify-center drip'
-      )}
-    >
+  componentDidMount() {
+    window.addEventListener('resize', this.updateWindow);
+    this.updateWindow();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindow);
+  }
+
+  updateWindow = () => {
+    const { small, large } = Global.breakpoints;
+    const currentBreakpoint =
+      window.innerWidth <= small.upperbound ? small.label : large.label;
+
+    if (this.state.currentBreakpoint !== currentBreakpoint)
+      this.setState({ currentBreakpoint });
+  };
+
+  render() {
+    const { block, z } = this.props;
+    const fields = get(block, 'fields', {});
+    const colorClass = `ImageText--${get(fields, 'backgroundColor', 'pink')}`;
+    const position =
+      this.state.currentBreakpoint === 'small'
+        ? 0
+        : get(fields, 'imagePosition', 0);
+    const positionX = get(fields, 'imagePositionX', 0);
+    const isReverseArrangement = get(fields, 'isReverseArrangement', false);
+    const imageTextRatio = get(fields, 'imageTextRatio', '40:60');
+    const textDesktopSize =
+      imageTextRatio === '50:50' ? 'md-col-5' : 'md-col-6';
+
+    return (
       <div
-        className={cx('flex container-width', styles['ImageText__container'], {
-          [styles['ImageText__container--reverse']]: isReverseArrangement
-        })}
+        style={{ zIndex: z }}
+        className={cx(
+          styles['ImageText'],
+          styles[colorClass],
+          'flex flex-row justify-center drip'
+        )}
       >
         <div
           className={cx(
-            styles['ImageText__text-content'],
-            'flex flex-column justify-center col-12 md-col-6'
+            'flex container-width',
+            styles['ImageText__container'],
+            {
+              [styles['ImageText__container--reverse']]: isReverseArrangement
+            }
           )}
         >
-          <h2 className="block-headline mb3">{get(fields, 'title', '')}</h2>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: marked(get(fields, 'text', ''))
+          <div
+            className={cx(
+              styles['ImageText__text-content'],
+              textDesktopSize,
+              'flex flex-column justify-center col-12'
+            )}
+          >
+            <h2 className="block-headline mb3">{get(fields, 'title', '')}</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: marked(get(fields, 'text', ''))
+              }}
+              className={cx(styles['ImageText__text'], 'block-subheadline')}
+            />
+          </div>
+          <Image
+            className={cx(
+              styles['ImageText__image'],
+              'z-overlay col-4 mt2 mx-auto'
+            )}
+            style={{
+              transform: `translate(${positionX}%, ${position}%)`
             }}
-            className="block-subheadline"
+            alt={`${get(fields, 'title', '')} illustration`}
+            src={contentfulImgUtil(
+              get(fields, 'image.fields.file.url', ''),
+              '1400',
+              'png'
+            )}
           />
         </div>
-        <Image
-          className={cx(styles['ImageText__image'], 'z-overlay col-4 mt2')}
-          style={{ transform: `translateY(${position}%)` }}
-          alt={`${get(fields, 'title', '')} illustration`}
-          src={contentfulImgUtil(
-            get(fields, 'image.fields.file.url', ''),
-            '1400',
-            'png'
-          )}
-        />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ImageText;
 
