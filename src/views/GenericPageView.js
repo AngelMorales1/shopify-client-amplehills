63471f2380ef4a66814ba9d543ae544d63b789fc
sohee Lim <limsohee1002@gmail.com>
2018-08-23 12:14:36 -0400

    Move GenericPageView hero to be Block as CenterImageText

--- src/views/GenericPageView.js
+++ src/views/GenericPageView.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import get from 'utils/get';
-import contentfulImgUtil from 'utils/contentfulImgUtil';
-import cx from 'classnames';
 
-import styles from './GenericPageView.scss';
 import BlockSwitch from 'components/BlockSwitch';
-import { Image } from 'components/base';
 
 class GenericPageView extends Component {
   render() {
