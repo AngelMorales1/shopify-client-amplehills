61b1d9176f76df993f1b0772fe26cf15b3880e34
sohee Lim <limsohee1002@gmail.com>
2018-08-23 15:40:58 -0400

    Get press data from contentful through GenericPageContainer

--- src/containers/GenericPageContainer.js
+++ src/containers/GenericPageContainer.js
@@ -2,6 +2,7 @@ import ContainerBase from 'lib/ContainerBase';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getGenericPage } from 'state/actions/genericPageActions';
+import { getPressData } from 'state/actions/pressActions';
 
 import get from 'utils/get';
 
@@ -9,10 +10,17 @@ class GenericPageContainer extends ContainerBase {
   view = import('views/GenericPageView');
 
   model = () => {
-    const { getGenericPage } = this.props.actions;
+    const { getGenericPage, getPressData } = this.props.actions;
     const { path } = this.props.match;
 
-    return getGenericPage(path);
+    return Promise.all([getGenericPage(), getPressData()]).then(
+      ([genericPage, press]) => {
+        return {
+          genericPage: get(genericPage, 'value'),
+          press: get(press, 'value')
+        };
+      }
+    );
   };
 }
 
@@ -30,7 +38,8 @@ const mapDispatchToProps = dispatch => {
   return {
     actions: bindActionCreators(
       {
-        getGenericPage
+        getGenericPage,
+        getPressData
       },
       dispatch
     )
