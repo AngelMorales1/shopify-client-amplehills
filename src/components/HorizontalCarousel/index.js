15a5d180ccd8ed9069caea51612741870e83f228
sohee Lim <limsohee1002@gmail.com>
2018-08-23 12:39:58 -0400

    Get data from contentful to HorizontalCarousel

--- src/components/HorizontalCarousel/index.js
+++ src/components/HorizontalCarousel/index.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import get from 'utils/get';
 import contentfulImgUtil from 'utils/contentfulImgUtil';
 import cx from 'classnames';
 
 import { Image, Button } from 'components/base';
 
-class HorizontalCarousel extends Component {
-  render() {
-    const { title, buttonLabel, buttonLink } = this.props;
-    return (
+const HorizontalCarousel = ({ block, z }) => {
+  const fields = get(block, 'fields', {});
+  const contentBlocks = get(fields, 'contentBlocks', []);
+
+  return (
+    <div className="bg-bees-wax">
       <div>
-        <div>
-          <span>{title}</span>
-          <Button to={buttonLink} label={buttonLabel} />
-        </div>
-        <div />
+        <span>{get(fields, 'title', '')}</span>
+        <Button label={get(fields, 'buttonLabel', '')} />
       </div>
-    );
-  }
-}
+      <div>
+        {contentBlocks.map(contentBlock => {
+          return <div>block</div>;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default HorizontalCarousel;
