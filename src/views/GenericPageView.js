eb3cef4d1c81ac722fa3a579961a0a53fae0aa98
sohee Lim <limsohee1002@gmail.com>
2018-08-23 09:23:32 -0400

    Move background color style to scss in GenericPAgeView

--- src/views/GenericPageView.js
+++ src/views/GenericPageView.js
@@ -3,6 +3,7 @@ import get from 'utils/get';
 import contentfulImgUtil from 'utils/contentfulImgUtil';
 import cx from 'classnames';
 
+import styles from './GenericPageView.scss';
 import BlockSwitch from 'components/BlockSwitch';
 import { Image } from 'components/base';
 
@@ -17,11 +18,10 @@ class GenericPageView extends Component {
     const image = get(fields, 'image', null);
     const color = get(fields, 'color', 'blue');
     const isDripOn = get(fields, 'drip', false);
-
+    const colorClass = `GenericPageView--${get(fields, 'color', 'blue')}`;
     const classes = cx(
+      styles[colorClass],
       {
-        'bg-iceberg': color === 'blue',
-        'bg-bees-wax': color === 'yellow',
         drip: isDripOn
       },
       'pb2 z-sub-nav'
@@ -45,7 +45,7 @@ class GenericPageView extends Component {
             ) : null}
           </div>
         </div>
-        <div className="drip-padding-on-children">
+        <div>
           {blocks &&
             blocks.map((block, i) => (
               <BlockSwitch
