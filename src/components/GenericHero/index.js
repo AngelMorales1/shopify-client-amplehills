24b61a6df6607214467963eeeeffc276774c097b
sohee Lim <limsohee1002@gmail.com>
2018-08-24 08:00:37 -0400

    Add PropTypes to GenericHero

--- src/components/GenericHero/index.js
+++ src/components/GenericHero/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import get from 'utils/get';
 import contentfulImgUtil from 'utils/contentfulImgUtil';
 import cx from 'classnames';
 
+import imageModel from 'models/imageModel';
 import styles from './GenericHero.scss';
 import { Image } from 'components/base';
 
@@ -41,3 +43,27 @@ const GenericHero = ({ block, z }) => {
 };
 
 export default GenericHero;
+
+GenericHero.propTypes = {
+  z: PropTypes.number,
+  block: PropTypes.shape({
+    fields: PropTypes.shape({
+      color: PropTypes.string,
+      drip: PropTypes.bool,
+      image1: imageModel.propTypes,
+      title: PropTypes.string
+    })
+  })
+};
+
+GenericHero.defaultProps = {
+  z: 1,
+  block: {
+    fields: PropTypes.shape({
+      color: 'blue',
+      drip: false,
+      image1: null,
+      title: ''
+    })
+  }
+};
