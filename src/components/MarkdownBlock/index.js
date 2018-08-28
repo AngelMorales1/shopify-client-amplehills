d39303254f7d9d71979e870ca1b0bf0f3c6d47d0
sohee Lim <limsohee1002@gmail.com>
2018-08-28 17:04:12 -0400

    Add title on left on MarkdownBlock

--- src/components/MarkdownBlock/index.js
+++ src/components/MarkdownBlock/index.js
@@ -7,17 +7,42 @@ import get from 'utils/get';
 import styles from './GenericPage.scss';
 
 const MarkdownBlock = props => {
-  const { z } = props;
-  const markdown = get(props, 'block.fields.content', '');
-  const isDripOn = get(props, 'block.fields.drip', false);
+  const { z, block } = props;
+  const fields = get(block, 'fields', {});
+  const markdown = get(fields, 'content', '');
+  const isDripOn = get(fields, 'drip', false);
+  const colorClass = `MarkdownBlock--${get(
+    fields,
+    'backgroundColor',
+    'white'
+  )}`;
+  const titleOnLeft = get(fields, 'titleLeft', false);
+  const title = get(fields, 'title', '');
 
   return (
-    <div style={{ zIndex: z }} className={cx('bg-white', { drip: isDripOn })}>
+    <div
+      style={{ zIndex: z }}
+      className={cx(styles[colorClass], 'flex justify-center', {
+        [styles['MarkdownBlock--title-left']]: titleOnLeft,
+        drip: isDripOn
+      })}
+    >
+      {titleOnLeft ? (
+        <div
+          className={cx(
+            styles['MarkdownBlock__title-container'],
+            'col-12 md-col-4 flex flex-row justify-center'
+          )}
+        >
+          <span className="block-headline">{title}</span>
+        </div>
+      ) : null}
       <div
         dangerouslySetInnerHTML={{ __html: marked(markdown) }}
         className={cx(
           styles['MarkdownBlock'],
-          'form-container-width transition-slide-up mx-auto px3 py4'
+          'form-container-width transition-slide-up mx-auto px3 py4',
+          { 'col-12 md-col-8': titleOnLeft }
         )}
       />
     </div>
