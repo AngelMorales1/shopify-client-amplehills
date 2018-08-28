76c1827a133c7ce981d30f5ed01e3516e843977a
sohee Lim <limsohee1002@gmail.com>
2018-08-28 08:10:56 -0400

    Change pressItemsModel to pressItemModel

--- src/models/pressItemModel.js
+++ src/models/pressItemModel.js
@@ -2,31 +2,27 @@ import PropTypes from 'prop-types';
 import imageModel from 'models/imageModel';
 
 export default {
-  propTypes: PropTypes.arrayOf(
-    PropTypes.shape({
-      fields: PropTypes.shape({
-        linkUrl: PropTypes.string,
-        logoImage: imageModel.propTypes,
-        quote: PropTypes.string,
-        title: PropTypes.string
-      }),
-      sys: PropTypes.shape({
-        id: PropTypes.string
-      })
+  propTypes: PropTypes.shape({
+    fields: PropTypes.shape({
+      linkUrl: PropTypes.string,
+      logoImage: imageModel.propTypes,
+      quote: PropTypes.string,
+      title: PropTypes.string
+    }),
+    sys: PropTypes.shape({
+      id: PropTypes.string
     })
-  ),
+  }),
 
-  default: [
-    {
-      fields: {
-        linkUrl: '',
-        logoImage: imageModel.default,
-        quote: '',
-        title: ''
-      },
-      sys: {
-        id: ''
-      }
+  default: {
+    fields: {
+      linkUrl: '',
+      logoImage: imageModel.default,
+      quote: '',
+      title: ''
+    },
+    sys: {
+      id: ''
     }
-  ]
+  }
 };
