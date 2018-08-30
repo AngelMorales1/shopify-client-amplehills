901637a25a7738d96bf41a6d4cbcdb8d02cde6bc
sohee Lim <limsohee1002@gmail.com>
2018-08-30 16:39:47 -0400

    Clean up

--- src/components/LocationsCards/index.js
+++ src/components/LocationsCards/index.js
@@ -126,7 +126,7 @@ class LocationsCards extends Component {
             'w100 flex flex-wrap items-start justify-center'
           )}
         >
-          {!!sortedLocations.length ? (
+          {sortedLocations.length ? (
             <div className="transition-slide-up w100">
               <div
                 className={cx(
