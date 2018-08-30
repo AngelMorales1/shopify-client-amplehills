5f62d71c652e45b3531cb57a2af435cfba4d2d90
sohee Lim <limsohee1002@gmail.com>
2018-08-30 14:29:03 -0400

    Add alertIsActive selector test

---
+++ src/tests/state/mocks/unselected/alertIsActive.js
@@ -0,0 +1,25 @@
+export default {
+  fields: {
+    alertCopy: 'Looking for more products and information?',
+    linkCopy: 'Check out our full site!',
+    linkUrl: 'https://amplehills.com',
+    title: 'Launch Alert'
+  },
+  sys: {
+    contentType: {
+      sys: {}
+    },
+    createdAt: '2018-08-23T15:24:31.499Z',
+    environment: {
+      sys: {}
+    },
+    id: '1VdumuIz4QKeMu2a8yOuiW',
+    locale: 'en-US',
+    revision: 1,
+    space: {
+      sys: {}
+    },
+    type: 'Entry',
+    updatedAt: '2018-08-23T15:24:31.499Z'
+  }
+};
