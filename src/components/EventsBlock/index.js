b05bf6de053e66bcae01d01da14aff06b365d872
sohee Lim <limsohee1002@gmail.com>
2018-09-04 19:26:26 -0400

    Add EventBlock style

--- src/components/EventsBlock/index.js
+++ src/components/EventsBlock/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
 
 import cx from 'classnames';
 import get from 'utils/get';
 
+import { Button } from 'components/base';
 import styles from './EventsBlock.scss';
 
-const EventsBlock = props => {
-  const { z, block } = props;
-  const fields = get(block, 'fields', {});
-  const isDripOn = get(fields, 'drip', false);
-  const colorClass = `EventsBlock--${get(fields, 'backgroundColor', 'white')}`;
-  const title = get(fields, 'title', '');
+class EventsBlock extends Component {
+  state = {};
+  render() {
+    const { z, block } = this.props;
+    const fields = get(block, 'fields', {});
+    const isDripOn = get(fields, 'drip', false);
+    const colorClass = `EventsBlock--${get(
+      fields,
+      'backgroundColor',
+      'white'
+    )}`;
+    const title = get(fields, 'title', '');
+    const text = get(fields, 'text', '');
 
-  return (
-    <div
-      style={{ zIndex: z }}
-      className={cx(styles[colorClass], 'flex justify-center', {
-        drip: isDripOn
-      })}
-    />
-  );
-};
+    return (
+      <div
+        style={{ zIndex: z }}
+        className={cx(
+          styles[colorClass],
+          'flex flex-column items-center justify-center',
+          {
+            drip: isDripOn
+          }
+        )}
+      >
+        <div className="px2 text-container-width center">
+          <h2 className="block-headline mt4 mb3">{title}</h2>
+          <span className="block-subheadline">{text}</span>
+        </div>
+      </div>
+    );
+  }
+}
 
 EventsBlock.propTypes = {
   z: PropTypes.number,
