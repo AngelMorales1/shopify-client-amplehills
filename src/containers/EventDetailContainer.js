594b6764775d76a0cd9dd21a6b3da579a12f3689
sohee Lim <limsohee1002@gmail.com>
2018-09-14 12:16:58 -0400

    Add event selector test

--- src/containers/EventDetailContainer.js
+++ src/containers/EventDetailContainer.js
@@ -24,6 +24,7 @@ class EventDetailContainer extends ContainerBase {
 }
 
 const mapStateToProps = (state, props) => {
+  console.log(event(state, props));
   return {
     checkout: checkout(state),
     addLineItemsStatus: get(state, 'status.addLineItemsStatus'),
