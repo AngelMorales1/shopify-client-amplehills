d9298d9fc445645de121cf3cbd0554d00f6910f9
sohee Lim <limsohee1002@gmail.com>
2018-09-25 08:21:31 -0400

    Clean up

--- src/state/selectors/pressItems.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-import get from 'utils/get';
-
-export default createSelector(
-  state => get(state, 'press', {}),
-  latestPressItems => get(latestPressItems, 'pressItems.items', {})
-);
