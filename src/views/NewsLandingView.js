3794ae5336d71f464e180cc15c0cd434b1576fb7
sohee Lim <limsohee1002@gmail.com>
2018-09-27 12:20:43 -0400

    Render different number of ArticlePreview by window size in NewsLandingView

--- src/views/NewsLandingView.js
+++ src/views/NewsLandingView.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import Global from 'constants/Global';
 import get from 'utils/get';
 
 import ArticlePreview from 'components/ArticlePreview';
 
 class NewsLandingView extends Component {
+  state = {
+    currentBreakpoint: Global.breakpoints.medium.label
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateWindow);
+    this.updateWindow();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindow);
+  }
+
+  updateWindow = () => {
+    const { small, medium } = Global.breakpoints;
+    const currentBreakpoint =
+      window.innerWidth <= medium.lowerbound ? small.label : medium.label;
+
+    if (this.state.currentBreakpoint !== currentBreakpoint)
+      this.setState({ currentBreakpoint });
+  };
+
   render() {
-    const articles = get(this, 'props.news.articles', []);
+    const renderArticleCount =
+      this.state.currentBreakpoint === Global.breakpoints.medium.label ? 3 : 5;
+    const articles = get(this, 'props.news.articles', []).slice(
+      0,
+      renderArticleCount
+    );
 
     return (
       <div className="flex flex-row justify-between py4 px2">
