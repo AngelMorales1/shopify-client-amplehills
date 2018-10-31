011be677d7b14367d4d90bb6556b43846bb1e574
sohee Lim <limsohee1002@gmail.com>
2018-10-31 09:26:15 -0400

    Add Filter button in InStore

--- src/components/InStores/index.js
+++ src/components/InStores/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
 import cx from 'classnames';
@@ -7,79 +7,106 @@ import get from 'utils/get';
 import { Button } from 'components/base';
 import styles from './InStores.scss';
 
-const InStores = ({ localRetailers, text }) => {
-  const localRetailersValues = Object.values(localRetailers);
-  const uniqueFilter = localRetailersValues.reduce(
-    (uniqueFilters, localRetailer) => {
-      const filter = localRetailer.filter.toUpperCase();
-      if (!uniqueFilters[filter]) {
-        uniqueFilters[filter] = true;
-      }
+class InStores extends Component {
+  state = {
+    activeFilter: ''
+  };
 
-      return uniqueFilters;
-    },
-    {}
-  );
+  handleFilterButtonClick = filter => {
+    if (filter === this.state.activeFilter) {
+      return this.setState({ activeFilter: '' });
+    }
 
-  return (
-    <div>
-      <div className="bg-iceberg py4 px3 flex flex-column justify-center items-center drip">
-        <h2 className="block-headline center mb3">Local Retailers</h2>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: marked(text)
-          }}
-          className="markdown-block center text-container-width"
-        />
-        <div className="flex flex-row">
-          {Object.keys(uniqueFilter).map(filter => (
-            <Button
-              className="m1"
-              color="madison-blue"
-              variant="primary-small"
-              label={filter}
-            />
-          ))}
+    return this.setState({ activeFilter: filter });
+  };
+
+  render() {
+    const { localRetailers, text } = this.props;
+    const { activeFilter } = this.state;
+    const localRetailersValues = Object.values(localRetailers);
+    const uniqueFilter = localRetailersValues.reduce(
+      (uniqueFilters, localRetailer) => {
+        const filter = localRetailer.filter.toUpperCase();
+        if (!uniqueFilters[filter]) {
+          uniqueFilters[filter] = true;
+        }
+
+        return uniqueFilters;
+      },
+      {}
+    );
+
+    const selectedLocalRetailers = activeFilter
+      ? localRetailersValues.filter(
+          retailer => retailer.filter.toUpperCase() === activeFilter
+        )
+      : localRetailersValues;
+
+    return (
+      <div>
+        <div className="bg-iceberg py4 px3 flex flex-column justify-center items-center drip">
+          <h2 className="block-headline center mb3">Local Retailers</h2>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: marked(text)
+            }}
+            className="markdown-block center text-container-width"
+          />
+          <div className="flex flex-row">
+            {Object.keys(uniqueFilter).map(filter => (
+              <Button
+                onClick={() => this.handleFilterButtonClick(filter)}
+                className="m1"
+                color={
+                  activeFilter === filter
+                    ? 'clear-madison-blue-border'
+                    : 'madison-blue'
+                }
+                variant="primary-small"
+                label={filter}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="mt3 py4 px3 flex flex-column items-center">
-        {localRetailersValues.map((localRetailer, i) => {
-          return (
-            <div
-              key={get(localRetailer, 'uuid', i)}
-              className={cx(
-                styles['InStores__local-retailer-container'],
-                'flex form-container-width justify-between w100 my2 p3'
-              )}
-            >
+        <div className="mt3 py4 px3 flex flex-column items-center">
+          {selectedLocalRetailers.map((localRetailer, i) => {
+            return (
               <div
+                key={get(localRetailer, 'uuid', i)}
                 className={cx(
-                  styles['InStores__local-retailer-text-container']
+                  styles['InStores__local-retailer-container'],
+                  'flex form-container-width justify-between w100 my2 p3'
                 )}
               >
-                <p className="bold mb1">{localRetailer.title}</p>
-                <p>{`${get(localRetailer, 'address', '')}, ${get(
-                  localRetailer,
-                  'city',
-                  ''
-                )}, ${get(localRetailer, 'state', '')} ${get(
-                  localRetailer,
-                  'zip',
-                  ''
-                )}`}</p>
+                <div
+                  className={cx(
+                    styles['InStores__local-retailer-text-container']
+                  )}
+                >
+                  <p className="bold mb1">{localRetailer.title}</p>
+                  <p>{`${get(localRetailer, 'address', '')}, ${get(
+                    localRetailer,
+                    'city',
+                    ''
+                  )}, ${get(localRetailer, 'state', '')} ${get(
+                    localRetailer,
+                    'zip',
+                    ''
+                  )}`}</p>
+                </div>
+                <Button
+                  className={cx(styles['InStores__local-retailer-button'])}
+                  variant="primary-small"
+                  color="peach"
+                  label={localRetailer.number}
+                />
               </div>
-              <Button
-                className={cx(styles['InStores__local-retailer-button'])}
-                variant="primary-small"
-                color="peach"
-                label={localRetailer.number}
-              />
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default InStores;
