799c6bb6daea5c534395879e789a693b06ecc7b2
Joshie Fishbein <joshie@sanctuary.computer>
2018-11-01 17:14:08 -0400

    add product type to Products selector, add products-by-type to products selector

---
+++ src/constants/ShopifyProductTypes.js
@@ -0,0 +1,12 @@
+/**
+ * This object is composed of references to strings that match names of
+ * possible `product_type` values represented on Shopify products
+ *
+ */
+
+export default {
+  'Ice Cream': 'Ice Cream',
+  Events: 'Events',
+  Classes: 'Classes',
+  'Party Deposit': 'Party Deposit'
+};
--- src/state/selectors/products.js
+++ src/state/selectors/products.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 import get from 'utils/get';
+import ShopifyProductTypes from 'constants/ShopifyProductTypes';
 
 export default createSelector(
   state => {
@@ -13,6 +14,11 @@ export default createSelector(
       const price = parseFloat(get(node, 'variants.edges[0].node.price', 0.0));
       const id = get(node, 'variants.edges[0].node.id', '');
       const handle = get(node, 'handle', '');
+      const type = get(
+        ShopifyProductTypes,
+        get(node, 'productType', null),
+        null
+      );
       const variants = get(node, 'variants.edges', []).map(variant => {
         const variantNode = get(variant, 'node', {});
         const { id, price, title, availableForSale } = variantNode;
@@ -26,7 +32,8 @@ export default createSelector(
         price,
         variants,
         handle,
-        available
+        available,
+        type
       };
 
       handlizedProducts[handle] = handlizedProduct;
@@ -37,75 +44,82 @@ export default createSelector(
   },
   state => get(state, 'products.contentfulProducts', []),
   (shopifyProducts, contentful) => {
+    console.log('SHOP', shopifyProducts);
     const products = get(contentful, 'items', []);
 
-    return products.reduce((mergedProducts, product) => {
-      const title = get(product, 'fields.productTitle', '');
-      const handle = get(product, 'fields.productHandle', '');
-      const description = get(product, 'fields.description', '');
-      const flavorDescription = get(product, 'fields.flavorDescription', '');
-      const gridImage = get(product, 'fields.image.fields.file.url', '');
-      const pintImage = get(product, 'fields.pintImage.fields.file.url', '');
-      const blocks = get(product, 'fields.contentBlocks', []);
-      const preOrderDate = get(product, 'fields.preOrderDate', false);
-      const cartDetails = get(product, 'fields.cartDetails', '');
-      const productHeroCarouselImages = get(
-        product,
-        'fields.productHeroCarouselImages',
-        []
-      );
-      const productHeroImage = get(
-        product,
-        'fields.productHeroImage.fields.file.url',
-        ''
-      );
-      const productHeroTitle = get(product, 'fields.productHeroTitle', '');
-      const productHeroAlert = get(product, 'fields.productHeroAlert', '');
-      const productHeroTitleBackgroundImage = get(
-        product,
-        'fields.productHeroTitleBackgroundImage.fields.file.url',
-        ''
-      );
-      const productHeroTitleBackgroundImagePosition = get(
-        product,
-        'fields.productHeroTitleBackgroundImagePosition',
-        0
-      );
-      const productHero = {
-        productHeroCarouselImages,
-        productHeroImage,
-        productHeroTitle,
-        productHeroTitleBackgroundImage,
-        productHeroTitleBackgroundImagePosition,
-        productHeroAlert
-      };
-      const whatsIncludedDrip = get(product, 'fields.whatsIncludedDrip', false);
-      const whatsIncludedIllustration = get(
-        product,
-        'fields.whatsIncludedIllustration.fields.file.url',
-        ''
-      );
-      const whatsIncludedProducts = get(
-        product,
-        'fields.whatsIncludedProducts',
-        []
-      );
-      const whatsIncluded = {
-        whatsIncludedDrip,
-        whatsIncludedIllustration,
-        whatsIncludedProducts
-      };
+    const mergedContentfulProducts = products.reduce(
+      (mergedProducts, product) => {
+        const title = get(product, 'fields.productTitle', '');
+        const handle = get(product, 'fields.productHandle', '');
+        const description = get(product, 'fields.description', '');
+        const flavorDescription = get(product, 'fields.flavorDescription', '');
+        const gridImage = get(product, 'fields.image.fields.file.url', '');
+        const pintImage = get(product, 'fields.pintImage.fields.file.url', '');
+        const blocks = get(product, 'fields.contentBlocks', []);
+        const preOrderDate = get(product, 'fields.preOrderDate', false);
+        const cartDetails = get(product, 'fields.cartDetails', '');
+        const productHeroCarouselImages = get(
+          product,
+          'fields.productHeroCarouselImages',
+          []
+        );
+        const productHeroImage = get(
+          product,
+          'fields.productHeroImage.fields.file.url',
+          ''
+        );
+        const productHeroTitle = get(product, 'fields.productHeroTitle', '');
+        const productHeroAlert = get(product, 'fields.productHeroAlert', '');
+        const productHeroTitleBackgroundImage = get(
+          product,
+          'fields.productHeroTitleBackgroundImage.fields.file.url',
+          ''
+        );
+        const productHeroTitleBackgroundImagePosition = get(
+          product,
+          'fields.productHeroTitleBackgroundImagePosition',
+          0
+        );
+        const productHero = {
+          productHeroCarouselImages,
+          productHeroImage,
+          productHeroTitle,
+          productHeroTitleBackgroundImage,
+          productHeroTitleBackgroundImagePosition,
+          productHeroAlert
+        };
+        const whatsIncludedDrip = get(
+          product,
+          'fields.whatsIncludedDrip',
+          false
+        );
+        const whatsIncludedIllustration = get(
+          product,
+          'fields.whatsIncludedIllustration.fields.file.url',
+          ''
+        );
+        const whatsIncludedProducts = get(
+          product,
+          'fields.whatsIncludedProducts',
+          []
+        );
+        const whatsIncluded = {
+          whatsIncludedDrip,
+          whatsIncludedIllustration,
+          whatsIncludedProducts
+        };
 
-      const shopifyProduct = get(shopifyProducts, handle, {
-        id: null,
-        price: null,
-        variants: [],
-        available: false
-      });
-      const subItems = get(product, 'fields.subItems', []).map(subItem => {
-        return get(subItem, 'fields.productHandle', '');
-      });
-      const subItemsAvailable =
+        const shopifyProduct = get(shopifyProducts, handle, {
+          id: null,
+          price: null,
+          variants: [],
+          type: null,
+          available: false
+        });
+        const subItems = get(product, 'fields.subItems', []).map(subItem => {
+          return get(subItem, 'fields.productHandle', '');
+        });
+        const subItemsAvailable =
         !subItems.length ||
         subItems.every(subItem => {
           const shopifySubItem = get(shopifyProducts, subItem, {});
@@ -134,7 +148,35 @@ export default createSelector(
         ...shopifyProduct
       };
 
-      return mergedProducts;
-    }, {});
+        return mergedProducts;
+      },
+      {}
+    );
+
+    const allProducts = Object.values(shopifyProducts).reduce(
+      (allMergedProducts, product) => {
+        const contentfulProduct = get(
+          mergedContentfulProducts,
+          product.handle,
+          null
+        );
+        const mergedProduct = contentfulProduct ? contentfulProduct : product;
+
+        if (mergedProduct.type) {
+          if (allMergedProducts[mergedProduct.type]) {
+            allMergedProducts[mergedProduct.type].push(mergedProduct);
+          } else {
+            allMergedProducts[mergedProduct.type] = [mergedProduct];
+          }
+        }
+
+        allMergedProducts[product.handle] = mergedProduct;
+
+        return allMergedProducts;
+      },
+      {}
+    );
+
+    return allProducts;
   }
 );
