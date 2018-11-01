6af54177b9780cba0f9e9036ff26a85d99ea48dc
sohee Lim <limsohee1002@gmail.com>
2018-11-01 15:49:59 -0400

    Add sytle to CardsBlock

--- src/components/CardsBlock/index.js
+++ src/components/CardsBlock/index.js
@@ -5,11 +5,67 @@ import contentfulImgUtil from 'utils/contentfulImgUtil';
 import cx from 'classnames';
 
 import styles from './CardsBlock.scss';
-import { Button } from 'components/base';
+import { Button, Image } from 'components/base';
 
-const CardsBlock = props => {
-  console.log(props);
-  return <div>cards block</div>;
+const CardsBlock = ({ cardsBlock }) => {
+  const cardBlock1 = get(cardsBlock, 'cardBlock1', null);
+  const cardBlock2 = get(cardsBlock, 'cardBlock2', null);
+
+  return (
+    <div
+      className={cx(
+        styles['CardsBlock'],
+        'w100 p3 mt4 flex content-width mx-auto'
+      )}
+    >
+      {cardBlock1 ? (
+        <div className="col-12 md-col-6">
+          <Button
+            to={cardBlock1.link}
+            className="text-white"
+            variant="style-none"
+          >
+            <div
+              className={cx(
+                styles['CardsBlock__card'],
+                styles[`CardsBlock__card--${cardBlock1.color}`],
+                'flex flex-column items-center justify-between p3 m1'
+              )}
+            >
+              <Image
+                className={cx(styles['CardsBlock__card-image'], 'col-4 mt2')}
+                src={cardBlock1.image}
+              />
+              <p className="py2">{cardBlock1.text}</p>
+            </div>
+          </Button>
+        </div>
+      ) : null}
+      {cardBlock2 ? (
+        <div className="col-12 md-col-6">
+          <Button
+            to={cardBlock2.link}
+            className="text-white"
+            variant="style-none"
+          >
+            <div
+              className={cx(
+                styles['CardsBlock__card'],
+                styles[`CardsBlock__card--${cardBlock2.color}`],
+                'flex flex-column items-center justify-between p3 m1'
+              )}
+            >
+              <Image
+                className={cx(styles['CardsBlock__card-image'], 'col-4 mt2')}
+                src={cardBlock2.image}
+              />
+              <p className="py2">{cardBlock2.text}</p>
+            </div>
+          </Button>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 CardsBlock.propTypes = {};
