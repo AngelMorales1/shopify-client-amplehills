2f5ded9e16685ab9d9345f6753e8b8033deb4d31
sohee Lim <limsohee1002@gmail.com>
2018-11-06 17:37:12 -0500

    Clean up dropdownNavUIActions

--- src/state/actions/ui/dropdownNavUIActions.js
+++ src/state/actions/ui/dropdownNavUIActions.js
@@ -1,26 +1,26 @@
 export const OPEN_SHOP_DROPDOWN = 'OPEN_SHOP_DROPDOWN';
-export const openShopDropdown = payload => {
+export const openShopDropdown = () => {
   return {
     type: OPEN_SHOP_DROPDOWN,
     payload: true
   };
 };
 export const CLOSE_SHOP_DROPDOWN = 'CLOSE_SHOP_DROPDOWN';
-export const closeShopDropdown = payload => {
+export const closeShopDropdown = () => {
   return {
     type: CLOSE_SHOP_DROPDOWN,
     payload: false
   };
 };
 export const OPEN_LOCATION_DROPDOWN = 'OPEN_LOCATION_DROPDOWN';
-export const openLocationDropdown = payload => {
+export const openLocationDropdown = () => {
   return {
     type: OPEN_LOCATION_DROPDOWN,
     payload: true
   };
 };
 export const CLOSE_LOCATION_DROPDOWN = 'CLOSE_LOCATION_DROPDOWN';
-export const closeLocationDropdown = payload => {
+export const closeLocationDropdown = () => {
   return {
     type: CLOSE_LOCATION_DROPDOWN,
     payload: false
