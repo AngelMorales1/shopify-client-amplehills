44412efc98df9744699c6d2daf17c5f2e5bc6e9e
Joshie Fishbein <joshie@sanctuary.computer>
2018-11-08 16:06:15 -0500

    add CakeRequestForm order building and validation

--- src/components/CakeRequestForm/index.js
+++ src/components/CakeRequestForm/index.js
@@ -3,23 +3,59 @@ import cx from 'classnames';
 import get from 'utils/get';
 
 import { Image, Button, TextField, FormFlash, Dropdown } from 'components/base';
+import styles from './CakeRequestForm.scss';
 
 class CakeRequestForm extends Component {
-  constructor(props) {
-    super(...arguments);
+  state = {
+    location: null,
+    name: null,
+    phone: null,
+    size: null,
+    flavor: null,
+    filling: null,
+    toppings: []
+  };
 
-    this.state = {
-      size: get(props, 'cakeDeposit.variants[0]', {})
-    };
-  }
+  handleLocationChange = location => this.setState({ location });
+  handleNameChange = name => this.setState({ name });
+  handlePhoneChange = phone => this.setState({ phone });
+  handleSizeChange = size => this.setState({ size });
+  handleFlavorChange = flavor => this.setState({ flavor });
+  handleFillingChange = filling => this.setState({ filling });
+
+  handleAddTopping = topping => {
+    const { toppings } = this.state;
+    if (toppings.includes(topping)) return null;
+
+    const newToppings = toppings.concat([topping]);
+    return this.setState({ toppings: newToppings });
+  };
+
+  handleRemoveTopping = topping => {
+    const { toppings } = this.state;
+    if (!toppings.includes(topping)) return null;
+
+    const newToppings = toppings.filter(
+      includedTopping => includedTopping !== topping
+    );
+    return this.setState({ toppings: newToppings });
+  };
+
+  openFlavorModal = () => this.setState({ flavorModalIsOpen: true });
+  closeFlavorModal = () => this.setState({ flavorModalIsOpen: false });
+  validateForm = () => Object.values(this.state).every(value => value !== null);
 
   render() {
+    const formIsValid = this.validateForm();
+
     const product = get(this, 'props.cakeDeposit', {});
     const variants = get(product, 'variants', []);
     const flavors = get(this, 'props.cakeFlavors', []);
     const fillings = get(this, 'props.cakeFillings', []);
     const toppings = get(this, 'props.cakeToppings', []);
     const locations = get(this, 'props.cakeLocations', {});
+
+    console.log(formIsValid, this.state);
 
     return (
       <div className="flex flex-wrap my4">
@@ -41,8 +77,8 @@ class CakeRequestForm extends Component {
               color="peach"
               variant="square"
               placeholder="Choose a Location"
-              value=""
-              onChange={filter => this.handleLocationChange(filter)}
+              value={get(this, 'state.location.value', null)}
+              onChange={this.handleLocationChange}
               options={Object.values(locations).map(location => ({
                 label: location.title,
                 value: location.id
@@ -56,10 +92,8 @@ class CakeRequestForm extends Component {
             <TextField
               className="w100 text-container-width"
               variant="square"
-              placeholder="Enter number of guests"
-              onChange={value =>
-                this.setState({ selectedNumberOfGuests: value })
-              }
+              placeholder="Enter a name"
+              onChange={this.handleNameChange}
             />
           </div>
           <div className="w100 mb4 flex flex-column items-center">
@@ -70,9 +104,7 @@ class CakeRequestForm extends Component {
               className="w100 text-container-width"
               variant="square"
               placeholder="Enter your phone number"
-              onChange={value =>
-                this.setState({ selectedNumberOfGuests: value })
-              }
+              onChange={this.handlePhoneChange}
             />
           </div>
 
@@ -96,7 +128,11 @@ class CakeRequestForm extends Component {
                     <Button
                       onClick={() => this.handleSizeChange(variant)}
                       className="center wh100"
-                      variant={'square'}
+                      variant={
+                        get(this, 'state.size.id', null) === id
+                          ? 'square--selected'
+                          : 'square'
+                      }
                     >
                       <div className="inline-flex flex-column w100 my2">
                         <p className="mb1 white-space-normal center">{title}</p>
@@ -110,12 +146,19 @@ class CakeRequestForm extends Component {
           </div>
           <div className="w100 mb4 flex flex-column items-center">
             <p className="bold big center mb2">Choose a flavor!</p>
+            <Button
+              variant="primary-small"
+              color="peach"
+              label="Need Help?"
+              className="uppercase mb3 tout"
+              onClick={this.openFlavorModal}
+            />
             <Dropdown
               className="w100 text-container-width"
               color="peach"
               variant="square"
               placeholder="Choose a Flavor"
-              value=""
+              value={get(this, 'state.flavor.value', null)}
               onChange={this.handleFlavorChange}
               options={flavors.map(flavor => ({
                 label: flavor.title,
@@ -128,14 +171,16 @@ class CakeRequestForm extends Component {
             <div className="form-container-width w100 flex flex-wrap justify-center">
               {fillings.map(filling => {
                 return (
-                  <div key={filling.uuid} className="col-3 p1">
+                  <div key={filling.title} className="col-3 p1">
                     <Button
                       className="center wh100 white-space-normal"
-                      variant={'square'}
-                      label={filling.title}
-                      onClick={() =>
-                        this.setState({ selectedTimeSlot: filling })
+                      variant={
+                        get(this, 'state.filling', null) === filling.title
+                          ? 'square--selected'
+                          : 'square'
                       }
+                      label={filling.title}
+                      onClick={() => this.handleFillingChange(filling.title)}
                     />
                   </div>
                 );
@@ -146,12 +191,23 @@ class CakeRequestForm extends Component {
             <p className="bold big center mb2">Add additional toppings!</p>
             <div className="form-container-width w100 flex flex-row flex-wrap justify-center">
               {toppings.map((topping, i) => {
+                const selectedToppings = this.state.toppings;
+                const toppingIsSelected = selectedToppings.includes(
+                  topping.title
+                );
+
                 return (
                   <div key={get(topping, 'id', i)} className="col-6 p1">
                     <Button
-                      onClick={() => this.handleAddOnClick(topping.title)}
-                      className="center wh100 "
-                      variant={'square'}
+                      onClick={
+                        toppingIsSelected
+                          ? () => this.handleRemoveTopping(topping.title)
+                          : () => this.handleAddTopping(topping.title)
+                      }
+                      className="center wh100"
+                      variant={
+                        toppingIsSelected ? 'square--selected' : 'square'
+                      }
                     >
                       <div className="inline-flex flex-column w100 my2">
                         <p className="mb1 white-space-normal center">
@@ -168,7 +224,130 @@ class CakeRequestForm extends Component {
             </div>
           </div>
         </div>
-        <div className="w100 bg-tuft-bush py4" />
+        <div className="w100 bg-tuft-bush py4">
+          <div className="max-width-3 mx-auto flex flex-wrap">
+            <div className="col-12 md-col-7 flex flex-column">
+              <span className="bold big mb3">Summary</span>
+              {formIsValid ? (
+                <div className="flex flex-column pr3">
+                  <span className="line-height">
+                    <span className="bold">Pickup Location: </span>
+                    {this.state.location.label}
+                  </span>
+                  <span className="line-height">
+                    <span className="bold">Name: </span>
+                    {this.state.name}
+                  </span>
+                  <span className="line-height">
+                    <span className="bold">Phone: </span>
+                    {this.state.phone}
+                  </span>
+                  <span className="line-height">
+                    <span className="bold">Size: </span>
+                    {this.state.size.title}
+                  </span>
+                  <span className="line-height">
+                    <span className="bold">Flavor: </span>
+                    {this.state.flavor.label}
+                  </span>
+                  <span className="line-height">
+                    <span className="bold">Filling: </span>
+                    {this.state.filling}
+                  </span>
+                  <span className="line-height">
+                    <span className="bold">Toppings: </span>
+                    {this.state.toppings.join(', ')}
+                  </span>
+                </div>
+              ) : (
+                <span className="line-height">Please fill out all fields.</span>
+              )}
+            </div>
+            <div className="col-12 md-col-5 flex flex-column items-start">
+              <span className="bold big mb3">
+                Deposit total{this.state.size
+                  ? `: $${this.state.size.price}`
+                  : ''}
+              </span>
+              <span className="line-height">
+                You will be asked to pay for any additional items or products
+                upon picking up the cake.
+              </span>
+              <Button
+                disabled={!formIsValid}
+                onClick={this.submitDeposit}
+                color="madison-blue"
+                className="mt3 inline-block w-auto"
+                label="Make Deposit"
+              />
+            </div>
+          </div>
+        </div>
+        {this.state.flavorModalIsOpen ? (
+          <div
+            className={cx(
+              'overflow-scroll fixed-cover bg-white-wash flex justify-center items-center transition-fade-in'
+            )}
+          >
+            <div
+              className={cx(
+                'wh100 modal modal--light-gray-border max-width-2 mx-auto relative flex items-center justify-center bg-white drop-shadow transition-slide-up-large-long'
+              )}
+            >
+              <div className="wh100 m-auto flex flex-column mb3 justify-center">
+                <div
+                  className={cx(
+                    styles['CakeRequestForm__modal__info'],
+                    'absolute t0 l0 w100 p3 overflow-scroll'
+                  )}
+                >
+                  <div className="mb3">
+                    <h3 className="callout mb1">
+                      Suggested Ice Cream Cake Flavors
+                    </h3>
+                    <p
+                      className={cx(
+                        styles['CakeRequestForm__modal__text'],
+                        'mb1'
+                      )}
+                    >
+                      Overwhelmed by options? You’re not alone. Check out our
+                      three most popular ice cream cake flavor pairings.
+                    </p>
+                  </div>
+                  {Object.values(flavors).map(flavor => {
+                    return (
+                      <div key={flavor.title} className="my3 pr3">
+                        <p className="bold mb1">{flavor.title}</p>
+                        <p
+                          className={cx(
+                            styles['CakeRequestForm__modal__text'],
+                            'mb1'
+                          )}
+                        >
+                          {flavor.description}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div
+                  className={cx(
+                    styles['CakeRequestForm__modal__close-bar'],
+                    'absolute b0 l0 w100 p1 bg-white'
+                  )}
+                >
+                  <Button
+                    className={cx('right')}
+                    color="madison-blue"
+                    label="Close"
+                    onClick={this.closeFlavorModal}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
