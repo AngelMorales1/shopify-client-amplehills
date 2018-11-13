ef0af2aa1b44e1eadf7521e9c85ca47aac3a59d1
sohee Lim <limsohee1002@gmail.com>
2018-11-13 12:31:11 -0500

    Add button, small title and big title to ImageDoubletext

--- src/components/ImageDoubleText/index.js
+++ src/components/ImageDoubleText/index.js
@@ -7,6 +7,7 @@ import contentfulImgUtil from 'utils/contentfulImgUtil';
 import imageModel from 'models/imageModel';
 
 import styles from './ImageDoubleText.scss';
+import { Button } from 'components/base';
 
 const ImageDoubleText = ({ block, z, setRef }) => {
   const fields = get(block, 'fields', {});
@@ -15,7 +16,17 @@ const ImageDoubleText = ({ block, z, setRef }) => {
     'backgroundColor',
     'white'
   )}`;
-  const isReverseArrangement = get(fields, 'isReverseArrangement', false);
+  const reverseArrangementX = get(fields, 'reverseArrangementX', false);
+  const reverseArrangementY = get(fields, 'reverseArrangementY', false);
+  const smallTitle = get(fields, 'smallTitle', '');
+  const smallTitleColor = get(fields, 'smallTitleColor', 'madison-blue');
+  const title = get(fields, 'title', '');
+  const title1 = get(fields, 'title1', '');
+  const text1 = get(fields, 'text1', '');
+  const title2 = get(fields, 'title2', '');
+  const text2 = get(fields, 'text2', '');
+  const buttonLabel = get(fields, 'buttonLabel', '');
+  const buttonLink = get(fields, 'buttonLink', '');
 
   return (
     <div
@@ -34,9 +45,9 @@ const ImageDoubleText = ({ block, z, setRef }) => {
             {
               [styles[
                 'ImageDoubleText--container--reverse'
-              ]]: isReverseArrangement
+              ]]: reverseArrangementX
             },
-            'container-width flex items-center justify-between'
+            'w100 container-width flex items-center justify-between'
           )}
         >
           <div
@@ -55,31 +66,55 @@ const ImageDoubleText = ({ block, z, setRef }) => {
           <div
             className={cx(
               styles['ImageDoubleText--text-container'],
-              'md-col-6'
+              'col-12 md-col-7 flex flex-column'
             )}
           >
-            <div className="mb3">
-              <h2 className="block-headline mb2">
-                {get(fields, 'title1', '')}
+            {smallTitle ? (
+              <p className={`text-${smallTitleColor} small-title`}>
+                {smallTitle}
+              </p>
+            ) : null}
+            {title ? (
+              <h2
+                className={cx(styles['ImageDoubleText__title'], 'carter my2')}
+              >
+                {title}
               </h2>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: marked(get(fields, 'text1', ''))
-                }}
-                className="markdown-block"
+            ) : null}
+            {title1 || text1 ? (
+              <div className="mb3">
+                <h2 className="block-headline mb2">
+                  {get(fields, 'title1', '')}
+                </h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: marked(get(fields, 'text1', ''))
+                  }}
+                  className="markdown-block"
+                />
+              </div>
+            ) : null}
+            {title2 || text2 ? (
+              <div>
+                <h2 className="block-headline mb2">
+                  {get(fields, 'title2', '')}
+                </h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: marked(get(fields, 'text2', ''))
+                  }}
+                  className="markdown-block"
+                />
+              </div>
+            ) : null}
+            {buttonLabel ? (
+              <Button
+                className="inline-flex mt2"
+                color="peach"
+                label={buttonLabel}
+                to={buttonLink}
               />
-            </div>
-            <div>
-              <h2 className="block-headline mb2">
-                {get(fields, 'title2', '')}
-              </h2>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: marked(get(fields, 'text2', ''))
-                }}
-                className="markdown-block"
-              />
-            </div>
+            ) : null}
           </div>
         </div>
       </div>
@@ -97,7 +132,7 @@ ImageDoubleText.propTypes = {
       title1: PropTypes.string,
       title2: PropTypes.string,
       image: imageModel.propTypes,
-      isReverseArrangement: PropTypes.bool
+      reverseArrangementX: PropTypes.bool
     })
   }),
   setRef: PropTypes.func
@@ -113,7 +148,7 @@ ImageDoubleText.defaultProps = {
       title1: '',
       title2: '',
       image: imageModel.default,
-      isReverseArrangement: false
+      reverseArrangementX: false
     }
   },
   setRef: () => {}
