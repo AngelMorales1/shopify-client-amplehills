f39c271b61c64947ffcc30d9437da7084a092686
Hugh Francis <hugh@sanctuary.computer>
2018-11-18 14:37:53 -0500

    Move over to new branch

--- api/availability.js
+++ api/availability.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-  res.end("Availability API");
+import timekit from './utils/timekit';
+
+module.exports = async (req, res) => {
+  try {
+    const app = await timekit.fetchAvailability();
+    return res.end(JSON.stringify(app.data));
+  } catch (e) {
+    return res.end(e.message || 'Unknown Error');
+  }
 };
---
+++ api/utils/timekit.js
@@ -0,0 +1,7 @@
+import timekit from 'timekit-sdk';
+
+timekit.configure({
+  appKey: process.env.TIMEKIT_API_KEY
+});
+
+export default timekit;
