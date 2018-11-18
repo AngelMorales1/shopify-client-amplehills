5b31ff313a6f6437baa366b81b286a4b4af2ad63
Hugh Francis <hugh@sanctuary.computer>
2018-11-18 15:29:49 -0500

    Typo

--- api/availability.js
+++ api/availability.js
@@ -4,18 +4,21 @@ const timekit = require('timekit-sdk');
 module.exports = async (req, res) => {
   try {
     timekit.configure({ appKey: process.env.TIMEKIT_API_KEY });
+
     const {
       query: { id, from, to, length }
     } = parse(req.url, true);
+
     const response = await timekit.fetchAvailability({
       resources: [id],
       from,
       to,
       length
     });
+
     return res.end(JSON.stringify(response.data));
   } catch (e) {
-    return res.end(JSON.parse(Object.keys(e)));
+    return res.end(JSON.stringify(Object.keys(e)));
     //return res.end((e && e.message) || 'Unknown Error');
   }
 };
