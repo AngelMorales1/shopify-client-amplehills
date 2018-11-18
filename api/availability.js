254b811ad3e7a8bf9e537171505d5070663bece2
Hugh Francis <hugh@sanctuary.computer>
2018-11-18 15:20:09 -0500

    Implement broad avail

--- api/availability.js
+++ api/availability.js
@@ -1,10 +1,18 @@
+const { parse } = require('url');
 const timekit = require('timekit-sdk');
 
 module.exports = async (req, res) => {
   try {
     timekit.configure({ appKey: process.env.TIMEKIT_API_KEY });
-
-    const response = await timekit.getApp();
+    const {
+      query: { id, from, to, length }
+    } = parse(req.url, true);
+    const response = await timekit.fetchAvailability({
+      resources: [id],
+      from,
+      to,
+      length
+    });
     return res.end(JSON.stringify(response.data));
   } catch (e) {
     return res.end((e && e.message) || 'Unknown Error');
--- api/resource.js
+++ api/resource.js
@@ -4,9 +4,10 @@ const timekit = require('timekit-sdk');
 module.exports = async (req, res) => {
   try {
     timekit.configure({ appKey: process.env.TIMEKIT_API_KEY });
-
-    const { query } = parse(req.url, true);
-    const response = await timekit.getResource({ id: query.id });
+    const {
+      query: { id }
+    } = parse(req.url, true);
+    const response = await timekit.getResource({ id });
     return res.end(JSON.stringify(response.data));
   } catch (e) {
     return res.end((e && e.message) || 'Unknown Error');
