cf28638734d82cd51d23881d339ad02c9c219e96
Hugh Francis <hugh@sanctuary.computer>
2018-11-18 14:47:26 -0500

    Fight

--- api/resource.js
+++ api/resource.js
@@ -1,12 +1,5 @@
 import timekit from './utils/timekit';
 
 module.exports = async (req, res) => {
-  return res.end(JSON.stringify(req));
-
-  try {
-    const app = await timekit.fetchAvailability();
-    return res.end(JSON.stringify(app.data));
-  } catch (e) {
-    return res.end(e.message || 'Unknown Error');
-  }
+  return res.end(JSON.stringify(Object.keys(req)));
 };
