50b9901ebfed190e91ccea979dbf33735b537e08
sohee Lim <limsohee1002@gmail.com>
2018-11-19 17:00:33 -0500

    Fix articleModel proptypes

--- src/models/articleModel.js
+++ src/models/articleModel.js
@@ -12,7 +12,7 @@ export default {
     handle: PropTypes.string,
     image: PropTypes.string,
     publishedAt: PropTypes.string,
-    tags: PropTypes.array
+    tags: PropTypes.object
   }),
   default: {
     id: '',
@@ -25,6 +25,6 @@ export default {
     handle: '',
     image: '',
     publishedAt: '',
-    tags: []
+    tags: {}
   }
 };
