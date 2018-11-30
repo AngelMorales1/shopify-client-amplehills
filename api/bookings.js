eec3ba5c32f35007e7c53854494df7c8f2278dde
Hugh Francis <hugh@sanctuary.computer>
2018-11-30 14:33:12 -0500

    Add Async

--- api/bookings.js
+++ api/bookings.js
@@ -1,3 +1,32 @@
-module.exports = (req, res) => {
-  res.end("Bookings API");
+const timekit = require('timekit-sdk');
+const access = require('access-control');
+const formatError = require('./utils/formatError');
+
+const cors = access({
+  origins: [
+    'https://www.amplehills.com',
+    'https://staging.amplehills.com',
+    'http://localhost:3000'
+  ],
+  methods: ['POST'],
+  credentials: false
+});
+
+module.exports = async (req, res) => {
+  if (cors(req, res)) return;
+
+  try {
+    timekit.configure({ appKey: process.env.TIMEKIT_API_KEY });
+
+    const response = await timekit.createBooking(JSON.parse(req.body));
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    return res.end(JSON.stringify(response.data));
+  } catch (e) {
+    res.writeHead((e && e.status) || 500, {
+      'Content-Type': 'application/json'
+    });
+    return res.end(JSON.stringify(formatError(e)));
+  }
 };
