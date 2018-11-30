a1a0ff99c513fcb4718d25869809b2b570f954bf
Hugh Francis <hugh@sanctuary.computer>
2018-11-30 16:55:12 -0500

    Complete feature

--- src/lib/Bookings.js
+++ src/lib/Bookings.js
@@ -1,10 +1,29 @@
 import QueryString from 'qs';
 
+const AMPLE_HILLS_API_ENDPOINT = 'https://ample-hills-api-g5qlhkswu.now.sh';
+
 const Bookings = {
   getAvailability(project_id) {
     const query = QueryString.stringify({ project_id, to: '180 days' });
     return window
-      .fetch(`https://ample-hills-api-7337le328.now.sh/availability?${query}`)
+      .fetch(`${AMPLE_HILLS_API_ENDPOINT}/availability?${query}`)
+      .then(response => {
+        if (!response.ok) {
+          throw response;
+        }
+        return response.json();
+      });
+  },
+
+  makeBooking(stringifiedTimekitRequestObject, customerName, customerEmail) {
+    return window
+      .fetch(`${AMPLE_HILLS_API_ENDPOINT}/bookings`, {
+        method: 'post',
+        body: JSON.stringify({
+          customer: { name: customerName, email: customerEmail },
+          ...JSON.parse(stringifiedTimekitRequestObject)
+        })
+      })
       .then(response => {
         if (!response.ok) {
           throw response;
