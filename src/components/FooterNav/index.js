4f629195f31797601678932cff6ae9a5cdd6ea09
sohee Lim <limsohee1002@gmail.com>
2018-12-04 17:08:35 -0500

    Fix FooterNav proptypes

--- src/components/FooterNav/index.js
+++ src/components/FooterNav/index.js
@@ -45,20 +45,23 @@ class FooterNav extends PureComponent {
 
 FooterNav.propTypes = {
   pathname: PropTypes.string,
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      simpleFragments: PropTypes.objectOf(
-        PropTypes.shape({
-          icon: PropTypes.shape({
-            data: PropTypes.string,
-            name: PropTypes.name
-          }),
-          uuid: PropTypes.string,
-          link: PropTypes.string,
-          text: PropTypes.string
-        })
-      )
-    })
+  items: PropTypes.oneOfType(
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        simpleFragments: PropTypes.objectOf(
+          PropTypes.shape({
+            icon: PropTypes.shape({
+              data: PropTypes.string,
+              name: PropTypes.name
+            }),
+            uuid: PropTypes.string,
+            link: PropTypes.string,
+            text: PropTypes.string
+          })
+        )
+      })
+    ),
+    PropTypes.object
   )
 };
 
