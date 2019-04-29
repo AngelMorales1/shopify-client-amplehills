80bf9cf448bc7b5103498bf5ac2e78cd06a6a42b
Joshie Fishbein <joshie@sanctuary.computer>
2019-04-29 15:04:17 -0400

    reset isProd()

--- src/utils/isProd.js
+++ src/utils/isProd.js
@@ -2,9 +2,8 @@ import getSubdomain from 'utils/getSubdomain';
 import Environments from 'constants/Environments';
 
 export default () => {
-  return true;
-  // return [
-  //   Environments.MVP.subdomain,
-  //   Environments.PRODUCTION.subdomain
-  // ].includes(getSubdomain());
+  return [
+    Environments.MVP.subdomain,
+    Environments.PRODUCTION.subdomain
+  ].includes(getSubdomain());
 };
